fd70a42047e87ff7b22ba9d53b8135854403c40e
Toshi Hondo <toshi.hondo@gmail.com>
2010-06-28 19:12:44 +0900

    add machine state, draft

--- src/libs/admachine/machinestatecontroller.cpp
+++ src/libs/admachine/machinestatecontroller.cpp
@@ -1,6 +1,42 @@
+//////////////////////////////////////////
+// Copyright (C) 2010 Toshinobu Hondo, Ph.D.
+// Science Liaison / Advanced Instrumentation Project
+//////////////////////////////////////////
+
 #include "machinestatecontroller.h"
 
-
-MachineStateController::MachineStateController()
+MachineStateController::MachineStateController() : state_(STATE_NONE)
+												 , posted_state_(state_)
 {
+}
+
+
+//static
+const wchar_t *
+MachineStateController::getStateString(MACHINE_STATE st)
+{
+  switch(st) {
+  STATE_NONE:   return "STATE_NONE";
+  OFF:          return "OFF";
+  INITIALIZING: return "INITIALIZING";
+	//INITIALIZED:  return "INITIALIZED";
+  DIAGNOSTIC_IN_PROGRESS: return "DIAGNOSTIC_IN_PROGRESS";
+  CALIBRATION_IN_PROGRESS: return "CALIBRATION_IN_PROGRESS";
+  MAINTENANCE_IN_PROGRESS: return "MAINTENANCE_IN_PROGRESS";
+  PREPARING_FOR_RUN:       return "PREPARING_FOR_RUN";
+  READY_FOR_RUN:           return "READY_FOR_RUN";
+  RUNNING:                 return "RUNNING";
+  STOP:                    return "STOP";
+	/*
+  DORMANT_STOP:            
+  DORMANT_INIT:            
+    DORMANT_HOlD            
+	BUSY                    // next method can not be accepted
+	ACQUIRING               // data acquisition in progress
+	READY_FOR_DOWNLOAD      // next method can be downloaded
+	WARNING                 // instrument is in warning state
+	ERROR                   // instrument is in error state, must be set STOP state too
+	*/
+  }
+  return L"";
 }
--- src/libs/admachine/machinestatecontroller.h
+++ src/libs/admachine/machinestatecontroller.h
@@ -10,7 +10,53 @@
 
 class ADMACHINESHARED_EXPORT MachineStateController {
 public:
-    MachineStateController();
+  enum MACHINE_STATE {
+	STATE_NONE              = 0x00000000,  // host software not initialized
+	OFF                     = 0x00000001,  // hardware software not initialized
+	INITIALIZING            = 0x00000002,  // hardware software loaded, initialize communication
+	STOP                    = 0x00000003,  // hardware software initialize complete | stopped
+	DIAGNOSTIC_IN_PROGRESS  = 0x00000004,  // power on initial diagnostic in progress.  User can't start initital conditioning
+	MAINTENANCE_IN_PROGRESS = 0x00000005,  // maintenance in progress (incl. MCP conditioning), User can be stared at initial conditioning
+	PREPARING_FOR_RUN       = 0x00000006,  // downloading method, GC oven temp goind down etc.
+	READY_FOR_RUN           = 0x00000007,  // initial condition running (pump is on)
+	RUNNING                 = 0x00000008,  // run in progress
+    DORMANT_INIT            = 0x00000009,  // <reset> to be INITRUN (back to inital condition and keep runngin)
+    DORMANT_HOlD            = 0x0000000a,  // <reset> to be HOLD (stay in current hardware condition and keep running)
+    DORMANT_STOP            = 0x0000000b,  // <reset> to be STOP (instrument stopped:= pump off, detectors off, HTV on Mass spec is off)
+	//--------- following states can be ored with above state --------
+	BUSY                    = 0x00002000,  // next method can not be accepted
+	ACQUIRING               = 0x00004000,  // data acquisition in progress
+	READY_FOR_DOWNLOAD      = 0x00008000,  // next method can be downloaded
+	WARNING                 = 0x40000000,  // instrument is in warning state
+	ERROR                   = 0x80000000,  // instrument is in error state, must be set STOP state too
+  };
+
+  enum MACHINE_COMMAND {
+	COMMAND_NONE,
+	COMMAND_CONNECT,
+	COMMAND_STOP,
+	COMMAND_INITRUN,
+	COMMAND_RUN,
+	COMMAND_PAUSE,
+	COMMAND_PAUSE_RERUN,
+	COMMAND_HOLD,
+	COMMAND_HOLD_RERUN,
+	COMMAND_REMOINJ,
+	COMMAND_MAX
+  };
+
+  enum SEQUENCE_STATE {
+	SEQUENCE_STATE_RESET,
+	SEQUENCE_STATE_RUNNING,
+	SEQUENCE_STATE_SUSPEND,
+	SEQUENCE_STATE_DORMANT,
+  };
+
+protected:
+  MACHINE_STATE state_;
+  MACHINE_STATE posted_state_;
+
+  MachineStateController();
 };
 
 #endif // MACHINESTATECONTROLLER_H
