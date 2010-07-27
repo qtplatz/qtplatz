af04b7fd85984d61b157f3bdc399e17fa28188c2
Toshi Hondo <toshi.hondo@gmail.com>
2010-07-27 15:24:15 +0900

    move adics project to under libs

--- src/libs/adics/main.cpp
+++ src/libs/adics/main.cpp
@@ -13,7 +13,9 @@
 #     pragma comment(lib, "TAO_AnyTypeCoded.lib")
 #     pragma comment(lib, "TAOd.lib")
 #     pragma comment(lib, "ACEd.lib")
-// #     pragma comment(lib, "interfaced.lib")
+#     pragma comment(lib, "adinterfaced.lib")
+#     pragma comment(lib, "adportabled.lib")
+#     pragma comment(lib, "acewrapperd.lib")
 #  else
 #     pragma comment(lib, "TAO_Utils.lib")
 #     pragma comment(lib, "TAO_PI.lib")
@@ -21,7 +23,9 @@
 #     pragma comment(lib, "TAO_AnyTypeCode.lib")
 #     pragma comment(lib, "TAO.lib")
 #     pragma comment(lib, "ACE.lib")
-// #     pragma comment(lib, "interface.lib")
+#     pragma comment(lib, "adinterface.lib")
+#     pragma comment(lib, "adportable.lib")
+#     pragma comment(lib, "acewrapper.lib")
 #  endif
 #endif
 
