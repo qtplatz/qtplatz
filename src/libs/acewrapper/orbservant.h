31e423653954743019c8d4bd536fd097f88c5e59
Toshi Hondo <toshi.hondo@gmail.com>
2010-11-02 14:14:53 +0900

    remove orbservant.cpp since it has moved and localized in servent plugin

--- src/libs/acewrapper/orbservant.h
+++ src/libs/acewrapper/orbservant.h
@@ -17,45 +17,6 @@ class TAO_ORB_Manager;
 
 namespace acewrapper {
 
-#if 0
-	class ORBServantManager {
-        ORBServantManager( const ORBServantManager& ); // noncopyable
-	public:
-        ~ORBServantManager();
-		ORBServantManager( CORBA::ORB_ptr orb = 0
-			          , PortableServer::POA_ptr = 0
-					  , PortableServer::POAManager_ptr = 0);
-
-		int init( int ac, ACE_TCHAR * av[] );
-		int fini();
-		void run();
-
-		CORBA::ORB_ptr orb();
-		PortableServer::POA_ptr root_poa();
-		PortableServer::POA_ptr child_poa();
-		PortableServer::POAManager_ptr poa_manager();
-
-		std::string activate( PortableServer::Servant );
-		void deactivate( const std::string& id );
-
-        bool spawn();
-
-		bool test_and_set_thread_flag();
-		static void * thread_entry( void * me );
-
-	protected:
-        size_t init_count_;
-		bool thread_running_;
-        ACE_Recursive_Thread_Mutex mutex_;
-		TAO_ORB_Manager * orbmgr_;
-	};
-
-	namespace singleton {
-		typedef ACE_Singleton< acewrapper::ORBServantManager, ACE_Recursive_Thread_Mutex > orbServantManager;
-	}
-#endif
-	//////////////////////////
-
 	template<class T> class ORBServant {
 	public:
 		~ORBServant() { 
