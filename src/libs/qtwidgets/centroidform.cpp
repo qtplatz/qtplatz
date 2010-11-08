c97ac53d7f22fa31420dad9e7f20ddbca0e4eec9
Toshi Hondo <toshi.hondo@gmail.com>
2010-11-08 20:42:55 +0900

    update centroidframe using QStandardItemModel

--- src/libs/qtwidgets/centroidform.cpp
+++ src/libs/qtwidgets/centroidform.cpp
@@ -5,7 +5,6 @@
 
 #include "centroidform.h"
 #include "ui_centroidform.h"
-//#include "standardmodel.h"
 #include <adcontrols/centroidmethod.h>
 #include <QStandardItemModel>
 
@@ -47,64 +46,39 @@ void
 CentroidForm::update_model()
 {
     QStandardItemModel& model = *model_;
+    adcontrols::CentroidMethod& method = *method_;
     // require 4 rows
     QStandardItem * rootNode = model.invisibleRootItem();
 
     if ( model.rowCount() == 0 ) {
-        //QList< QStandardITem * > rowItems;
-        //rowItems << new QStandardItem( "Centroid" );
-        //rowItems << new QStandardItem( "value" );
+
         rootNode->setColumnCount(2);
 
         QStandardItem * scanType = new QStandardItem( "ScanType" );
         rootNode->appendRow( scanType );
-        ui->treeView->expand( scanType->index() );
-        
         do {
             scanType->appendRow( new QStandardItem( "Peak Width [Da]" ) );
             scanType->appendRow( new QStandardItem( "Proportional [ppm]" ) );
             scanType->appendRow( new QStandardItem( "Constant [Da]" ) );
         } while(0);
 
+        model.insertColumn( 1, scanType->index() );
+
+        ui->treeView->expand( scanType->index() );
+        ui->treeView->setColumnWidth( 0, 200 );
+
         rootNode->appendRow( new QStandardItem( "Area/Height" ) );
         rootNode->appendRow( new QStandardItem( "Baseline Width [Da]" ) );
         rootNode->appendRow( new QStandardItem( "Peak Centroid Fraction [%]" ) );
     }
-    model.setItem( 0, 1, new QStandardItem( "xxx" ) );
-    model.setItem( 1, 1, new QStandardItem( "yyyy" ) );
-/*
-    int row = 0;
-    QModelIndex parent = model.index( row, 0 ); // parent
-    model.setData( parent, "ScanType" );
-    // choice of "TOF | Proportional | Constant"
-    model.setData( model.index( row, 1 ), "TOF" );
 
-    do {
-        int childRow = model.rowCount( parent );
-        if ( childRow < 3 )
-            model.insertRows( 0, 3 - childRow, parent );
-        //
-        model.setData( model.index( 0, 0, parent ), "Peak Width [Da]" );
-        model.setData( model.index( 0, 1, parent ), "0.10000" );      
-        //
-        model.setData( model.index( 1, 0, parent ), "Proportional[ppm]" );
-        model.setData( model.index( 1, 1, parent ), "200.0000" );
-        //
-        model.setData( model.index( 2, 0, parent ), "Constant[Da]" );
-        model.setData( model.index( 2, 1, parent ), "1.0000" );
+    model.setData( model.index( 0, 1, model.item( 0, 0 )->index() ), method.rsTofInDa() );
+    model.setData( model.index( 1, 1, model.item( 0, 0 )->index() ), method.rsPropoInPpm() );
+    model.setData( model.index( 2, 1, model.item( 0, 0 )->index() ), method.rsConstInDa() );
+    
+    model.setItem( 0, 1, new QStandardItem( "TOF" ) );
+    model.setItem( 1, 1, new QStandardItem( method.centroidAreaIntensity() ? "Area" : "Height" ) );
 
-        ui->treeView->expand( parent );
-    } while(0);
-    ui->treeView->setColumnWidth( 0, 200 );
-
-    //
-    model.setData( model.index( ++row, 0 ), "Area/Height" );
-    model.setData( model.index( row, 1 ), "Area" );
-    // 
-    model.setData( model.index( ++row, 0 ), "Baseline Width [Da]" );
-    model.setData( model.index( row, 1 ), "500.0000" );
-    // 
-    model.setData( model.index( ++row, 0 ), "Peak Centroid Fraction [%]" );
-    model.setData( model.index( row, 1 ), "50.0000" );
-*/
+    model.setData( model.index( 2, 1 ), method.baselineWidth() );
+    model.setData( model.index( 3, 1 ), method.peakCentroidFraction() * 100 );
 }
