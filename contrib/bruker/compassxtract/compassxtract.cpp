ae3378a82172c78fb70899e299fe563a3f7a685f
Toshi Hondo, Ph.D <toshi.hondo@scienceliaison.com>
2012-04-21 17:28:25 +0900

    add placefolder for CompassXtract accessor

---
+++ contrib/bruker/compassxtract/compassxtract.cpp
@@ -0,0 +1,54 @@
+/**************************************************************************
+** Copyright (C) 2010-2012 Toshinobu Hondo, Ph.D.
+** Science Liaison / Advanced Instrumentation Project
+*
+** Contact: toshi.hondo@scienceliaison.com
+**
+** Commercial Usage
+**
+** Licensees holding valid ScienceLiaison commercial licenses may use this file in
+** accordance with the ScienceLiaison Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and ScienceLiaison.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.TXT included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
+#include "compassxtract.hpp"
+#include "datafile_factory.hpp"
+
+#if defined WIN32
+#  if defined _DEBUG
+#     pragma comment(lib, "adcontrolsd.lib")
+#     pragma comment(lib, "portfoliod.lib")
+#  else
+#     pragma comment(lib, "adcontrols.lib")
+#     pragma comment(lib, "portfolio.lib")
+#  endif
+#endif
+
+Compassxtract::Compassxtract()
+{
+}
+
+namespace adcontrols {
+    class datafile_factory;
+}
+
+extern "C" {
+	COMPASSXTRACTSHARED_EXPORT adcontrols::datafile_factory * datafile_factory();
+}
+
+adcontrols::datafile_factory *
+datafile_factory()
+{
+	return new compassxtract::datafile_factory();
+}
---
+++ contrib/bruker/compassxtract/compassxtract.pro
@@ -0,0 +1,47 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-04-21T09:34:34
+#
+#-------------------------------------------------
+
+QT       -= core gui
+
+include(../../contrib.pri)
+
+#TARGET = compassxtract
+TARGET = $$qtLibraryTarget($$TARGET)
+TEMPLATE = lib
+
+INCLUDEPATH += .
+INCLUDEPATH += $$QTPLATZ_SOURCE_TREE/src
+DESTDIR = $$QTPLATZ_PLUGIN_PATH/$$PROVIDER
+
+DEFINES += COMPASSXTRACT_LIBRARY
+
+SOURCES += compassxtract.cpp \
+    datafile_factory.cpp \
+    datafile.cpp
+
+HEADERS += compassxtract.hpp\
+        compassxtract_global.hpp \
+    datafile_factory.hpp \
+    datafile.hpp
+
+symbian {
+    MMP_RULES += EXPORTUNFROZEN
+    TARGET.UID3 = 0xEF194CC6
+    TARGET.CAPABILITY = 
+    TARGET.EPOCALLOWDLLDATA = 1
+    addFiles.sources = compassxtract.dll
+    addFiles.path = !:/sys/bin
+    DEPLOYMENT += addFiles
+}
+
+unix:!symbian {
+    maemo5 {
+        target.path = /opt/usr/lib
+    } else {
+        target.path = /usr/lib
+    }
+    INSTALLS += target
+}
