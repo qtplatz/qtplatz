f91d3c44760ffa4c7b79b7f36ae5017d5762213d
Toshi Hondo <toshi.hondo@scienceliaison.com>
2013-01-15 08:41:27 +0900

    add cstring include for g++ compatibility

--- src/plugins/sequence/sequencedelegate.cpp
+++ src/plugins/sequence/sequencedelegate.cpp
@@ -27,47 +27,48 @@
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
 #include <algorithm>
+#include <cstring>
 
 namespace {
-	static const char * list[] = { "UNK", "STD", "QC" };
+    static const char * list[] = { "UNK", "STD", "QC" };
 
-	struct sample_type {
-		static const char * name( /* adsequence::SAMPLE_TYPE */ int typ ) {
-		    if ( size_t( typ ) < sizeof( list ) / sizeof( list[0] ) )
-				return list[ typ ];
-			return "unknown";
-		}
-		static adsequence::SAMPLE_TYPE value( const char * name ) {
-			BOOST_FOREACH( const char * a, list ) {
-				if ( std::strcmp( a, name ) == 0 )
-					return static_cast< adsequence::SAMPLE_TYPE >( std::distance( list[0], a ) );
-			}
+    struct sample_type {
+        static const char * name( /* adsequence::SAMPLE_TYPE */ int typ ) {
+            if ( size_t( typ ) < sizeof( list ) / sizeof( list[0] ) )
+                return list[ typ ];
+            return "unknown";
+        }
+        static adsequence::SAMPLE_TYPE value( const char * name ) {
+            BOOST_FOREACH( const char * a, list ) {
+                if ( std::strcmp( a, name ) == 0 )
+                    return static_cast< adsequence::SAMPLE_TYPE >( std::distance( list[0], a ) );
+            }
             return adsequence::SAMPLE_TYPE_UNKNOWN;
-		}
-	};
+        }
+    };
 
     static struct string_format_t {
-		const char * variable_name;
-		const char * format;
-	} string_format [] = {
-		{ "injvol", "%.1lf" }, { "run_length", "%.2lf" }
-	};
+        const char * variable_name;
+        const char * format;
+    } string_format [] = {
+        { "injvol", "%.1lf" }, { "run_length", "%.2lf" }
+    };
 
-	struct double_value {
-		static std::string toString( const double& value, const std::string& variable ) {
-			BOOST_FOREACH( const string_format_t& a, string_format ) {
-				if ( variable == a.variable_name )
-					return ( boost::format( a.format ) % value ).str();
-			}
-			return ( boost::format( "%.3lf" ) % value ).str();
-		}
-	};
+    struct double_value {
+        static std::string toString( const double& value, const std::string& variable ) {
+            BOOST_FOREACH( const string_format_t& a, string_format ) {
+                if ( variable == a.variable_name )
+                    return ( boost::format( a.format ) % value ).str();
+            }
+            return ( boost::format( "%.3lf" ) % value ).str();
+        }
+    };
 }
 
 using namespace sequence;
 
 SequenceDelegate::SequenceDelegate( QObject *parent ) : QItemDelegate( parent )
-	                                                  , schema_( new adsequence::schema )
+                                                      , schema_( new adsequence::schema )
 {
 }
 
@@ -82,23 +83,23 @@ SequenceDelegate::createEditor( QWidget *parent
 void
 SequenceDelegate::paint(QPainter * painter, const QStyleOptionViewItem& option, const QModelIndex& index ) const
 {
-	if ( schema_ && schema_->size() > size_t( index.column() ) ) {
+    if ( schema_ && schema_->size() > size_t( index.column() ) ) {
 
-		const adsequence::column& column = (*schema_)[ index.column() ];
+        const adsequence::column& column = (*schema_)[ index.column() ];
 
-		if ( column.type() == adsequence::COLUMN_SAMPLE_TYPE ) {
-			drawDisplay( painter, option, option.rect, sample_type::name( index.data( Qt::EditRole ).toInt() ) );
-		} else if ( column.type() == adsequence::COLUMN_INT ) {
-			QItemDelegate::paint( painter, option, index );
-		} else if ( column.type() == adsequence::COLUMN_DOUBLE ) {
-			double value = index.data( Qt::EditRole ).toDouble();
-			drawDisplay( painter, option, option.rect, double_value::toString( value, column.name() ).c_str() );
-		} else if ( column.type() == adsequence::COLUMN_VARCHAR ) {
-			QItemDelegate::paint( painter, option, index );
-		}
-		return;
-	}
-	QItemDelegate::paint( painter, option, index );
+        if ( column.type() == adsequence::COLUMN_SAMPLE_TYPE ) {
+            drawDisplay( painter, option, option.rect, sample_type::name( index.data( Qt::EditRole ).toInt() ) );
+        } else if ( column.type() == adsequence::COLUMN_INT ) {
+            QItemDelegate::paint( painter, option, index );
+        } else if ( column.type() == adsequence::COLUMN_DOUBLE ) {
+            double value = index.data( Qt::EditRole ).toDouble();
+            drawDisplay( painter, option, option.rect, double_value::toString( value, column.name() ).c_str() );
+        } else if ( column.type() == adsequence::COLUMN_VARCHAR ) {
+            QItemDelegate::paint( painter, option, index );
+        }
+        return;
+    }
+    QItemDelegate::paint( painter, option, index );
 }
 
 void
@@ -113,7 +114,7 @@ SequenceDelegate::setModelData( QWidget * editor
                                   , const QModelIndex& index ) const
 {
     QItemDelegate::setModelData( editor, model, index );
-	emit valueChanged( index );
+    emit valueChanged( index );
 }
 
 void
@@ -128,11 +129,11 @@ SequenceDelegate::updateEditorGeometry( QWidget * editor
 void
 SequenceDelegate::schema( const adsequence::schema& schema )
 {
-	schema_.reset( new adsequence::schema( schema ) );
+    schema_.reset( new adsequence::schema( schema ) );
 }
 
 const adsequence::schema&
 SequenceDelegate::schema() const
 {
-	return * schema_;
+    return * schema_;
 }
