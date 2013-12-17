78efc3ca0fb153c0fa59a587c181175c316b10c9
Toshi Hondo, Ph.D <toshi.hondo@qtplatz.com>
2013-12-17 09:05:10 +0900

    improved qt5vars.bat

--- qt5vars.bat
+++ qt5vars.bat
@@ -1,43 +1,78 @@
 @echo off
+set ARCH=
+set VC=
 
-set QTVER=5.2.0
-set VC=2012
+echo "##### " ARCH_VC=%ARCH% %VC%
+echo "args: " %1 %2 %3
 
-if "%2"=="vc12" set VC=2013
+if "%1"=="x64" (
+  set ARCH=x64
+) else (
+  set ARCH=x86
+)
 
-if "%1"=="x64" goto ARCH_x86_64
+if "%2"=="vc12" (
+  set VC=vc12
+  set QMAKESPEC=win32-msvc2013
+) else (
+  set VC=vc11
+  set QMAKESPEC=win32-msvc2012
+)
 
-:ARCH_x86_32
-set ARCH=x32
+set uname=%ARCH%_%VC%
+echo -------------------------------------
+echo ARCH=%ARCH%
+echo VC=%VC%
+echo arch_tool=%uname%
+echo QMAKESPEC=%QMAKESPEC%
+echo -------------------------------------
+
+if %uname%==x86_vc11 goto x86_vc11
+if %uname%==x86_vc12 goto x86_vc12
+if %uname%==x64_vc11 goto x64_vc11
+if %uname%==x64_vc12 goto x64_vc12
+
+:x86_vc11
+echo ======= setup for x86 32bit memory using VS2012 =================
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 set ACE_ROOT="%USERPROFILE%\src\vc11\ACE_wrappers"
-set QTDIR=C:\Qt\Qt%QTVER%\%QTVER%\msvc2012
+set QTDIR=C:\Qt\Qt5.1.1\5.1.1\msvc2012
 set QWT=C:\Qwt-6.1.1-svn
 set BOOST_ROOT=C:\Boost
-goto ARCH_DONE
+set RDBASE=
+goto all_set
 
-:ARCH_x86_64
-set ARCH=x64
+:x86_vc12
+echo ======= setup for x86 32bit memory using VS2013 =================
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+set ACE_ROOT="%USERPROFILE%\src\vc11\ACE_wrappers"
+set QTDIR=C:\x86\qt5\qtbase
+set QWT=C:\x86\vc12\Qwt-6.1.1-svn
+set BOOST_ROOT=C:\Boost
+set RDBASE=
+goto all_set
+
+:x64_vc11
+echo ======= setup for x64 64bit memory using VS2012 =================
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
 set ACE_ROOT="%USERPROFILE%\src64\ACE_wrappers"
 set QTDIR=C:\x64\qt5\qtbase
-set QWT=C:\x64\Qwt-6.1.1-svn
+set QWT=C:\x64\vc11\Qwt-6.1.1-svn
 set BOOST_ROOT=C:\x64\Boost
+goto all_set
 
-:ARCH_DONE
+:x64_vc12
+echo ======= setup for x64 64bit memory using VS2013 =================
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+set ACE_ROOT="%USERPROFILE%\src64\ACE_wrappers"
+set QTDIR=C:\x64\qt5\qtbase
+set QWT=C:\x64\vc12\Qwt-6.1.1-svn
+set BOOST_ROOT=C:\x64\Boost
+goto all_set
+
+:all_set
 
 set PATH=%ACE_ROOT%\lib;%ACE_ROOT%\bin;%QTDIR%\BIN;%PATH%
-
-if "%VC%"=="2013" goto VC2013
-:VC2012
-
-set QMAKESPEC=win32-msvc2012
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" %ARCH%
-goto VCVARS_DONE
-
-:VC2013
-set QMAKESPEC=win32-msvc2013
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %ARCH%
-
-:VCVARS_DONE
 
 echo -- QTDIR set to %QTDIR%
 echo -- QMAKESPEC set to %QMAKESPEC%
@@ -45,7 +80,4 @@ echo -- ACE_ROOT set to %ACE_ROOT%
 echo -- QWT set to %QWT%
 echo -- BOOST_ROOT set to %BOOST_ROOT%
 
-if not "%1"=="vsstart" goto ENDVSSTART
-devenv /useenv
-:ENDVSSTART
-
+echo -- all set.
