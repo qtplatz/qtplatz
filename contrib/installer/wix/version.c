3c44d14f7437e1ca7ad1f44639b80624fabc72db
Toshi Hondo, Ph.D <toshi.hondo@qtplatz.com>
2014-02-05 15:27:41 +0900

    add version.c for wix

---
+++ contrib/installer/wix/version.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+static char __product_revision__[] = "$Revision: 1.0 $";
+static char __product_release_ident__[] = "$Id: c938e6c8207b91ebc66ebe930d65c93643e0688a $";
+*/
+
+main(int ac, char * av[])
+{
+    int c, n;
+    int v[4];
+
+    memset(v, 0, sizeof(v));
+    n = 0;
+    while ( (c = getchar()) != EOF && !isdigit(c) )
+        ;
+    ungetc( c, stdin );
+    while ( (c = getchar()) != EOF ) {
+        if ( isdigit( c ) ) 
+            v[n] = v[n] * 10 + (c - '0');
+        else
+            ++n;
+        if ( n >= 4 )
+            break;
+    }
+    printf("<Include>\n<?define ProductVersion =\"%d.%d.%d.%d\" ?>\n</Include>", v[0], v[1], v[2], v[3]);
+}
+
