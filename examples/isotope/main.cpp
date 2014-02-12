a0e02725b6588d6ce096f0b791f1ff9e12a3beb1
Toshinobu Hondo <toshi.hondo@scienceliaison.com>
2014-02-12 14:30:58 +0900

    starting point for isotope re-implementation

--- examples/isotope/main.cpp
+++ examples/isotope/main.cpp
@@ -22,105 +22,43 @@
 **
 **************************************************************************/
 
-#include "tableofelement.hpp"
-#include "element.hpp"
-#include <string>
-#include <vector>
-#include <iostream>
-#include <cctype>
-#include <algorithm>
-
-#if defined _MSC_VER
-# pragma warning(disable:4100)
-#endif
-
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix.hpp>
-#include <boost/fusion/tuple.hpp>
-#include <map>
-
-typedef std::pair< element, int > element_type; // symbol, number of element, ex. C2 (two carbon atoms)
-
-namespace detail {
-
-    const char * element_dictionary [] = { "C", "H", "N", "O", "S", "P", "Cl" }; // just for quick test for peptides
-
-    typedef std::map< const char *, int > comp_type;
-
-    void comp_add( comp_type& comp, const std::pair< const char *, int >& c ) {
-        comp[ c.first ] += c.second;
-    }
-    void comp_join( comp_type& comp, comp_type& a ) {
-        for ( auto& c: a )
-            comp[ c.first ] += c.second;
-    }
-    void comp_repeat( comp_type& comp, int n ) {
-        for ( auto& c: comp )
-            c.second *= n;
-    }
-
-    namespace qi = boost::spirit::qi;
-    using boost::phoenix::bind;
-    using boost::spirit::qi::_val;
-    using boost::spirit::qi::_1;
-    using boost::spirit::ascii::space;
-
-    template<typename Iterator = std::string::const_iterator>
-    struct chemical_formula_parser : boost::spirit::qi::grammar<Iterator, comp_type() > {
-        
-        chemical_formula_parser() : chemical_formula_parser::base_type( molecule )
-                                  , element( element_dictionary, element_dictionary )  {
-            molecule =
-                + (
-                    atoms            [ boost::phoenix::bind(&comp_add, _val, qi::_1) ]
-                    | repeated_group [ boost::phoenix::bind(&comp_join, _val, qi::_1 ) ]
-                    | qi::space
-                    )
-                ;
-            // remark: this grammer does not handle isotopes, (ex. "18O" does recognize as heavy oxygen)
-            atoms = 
-                element >> ( qi::uint_ | qi::attr(1u) ) // default to 1, 
-                ;
-            repeated_group %= // forces attr proparation
-                '(' >> molecule >> ')'
-                    >> qi::omit[ qi::uint_[ boost::phoenix::bind( comp_repeat, qi::_val, qi::_1 ) ] ]
-                ;
-        }
-        qi::rule<Iterator, std::pair< const char *, int >() > atoms;
-        qi::rule<Iterator, comp_type()> molecule, repeated_group;
-        qi::symbols<char, const char *> element;
-    };
-}
-
-bool
-scanner( std::string& line, std::vector< element_type >& mol )
-{
-    std::string::const_iterator it = line.begin();
-    std::string::const_iterator end = line.end();
-
-    detail::chemical_formula_parser< std::string::const_iterator > parser;
-/*
-    detail::comp_type composition;
-    if ( boost::spirit::qi::parse( it, end, parser, composition ) && it == end ) {
-        
-    }
-*/  
-    return true;
-}
+#include "formula_parser.hpp"
 
 int
-main(int argc, char *argv[])
+main(int argc, char * argv[])
 {
     (void)argc;
     (void)argv;
 
-    std::string line;
+    std::string str;
+    std::wstring wstr;
+    
+    chem::chemical_formula_parser< std::string::const_iterator> cf;
 
-    while ( std::getline( std::cin, line ) ) {
-        if ( line.empty() || line[ 0 ] == 'q' || line[0] == 'Q')
+    while (std::getline(std::cin, str))  {
+        if (str.empty() || str[0] == 'q' || str[0] == 'Q')
             break;
 
-        std::vector< element_type > mol;
-        scanner( line, mol );
+        chem::map_type map;
+        std::string::const_iterator it = str.begin();
+        std::string::const_iterator end = str.end();
+        
+        if ( boost::spirit::qi::parse( it, end, cf, map ) && it == end ) {
+            std::cout << "-------------------------\n";
+            std::cout << "Parsing succeeded\n";
+            std::cout << str << " Parses OK: " << std::endl;
+            std::cout << str << " map size: " << map.size() << std::endl;
+            
+            for ( auto e: map )
+                std::cout << e.first.first << " "  << e.first.second  << " " << e.second << std::endl;
+            std::cout << "-------------------------\n";
+        } else {
+            std::cout << "-------------------------\n";
+            std::cout << "Parsing failed\n";
+            std::cout << "-------------------------\n";
+        }
     }
+    std::cout << "Bye... :-) \n\n";
+	return 0;
 }
+
