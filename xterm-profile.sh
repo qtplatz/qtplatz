2c21037b77e7f7a361faeabe014ed6de225cdd8f
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2014-10-12 06:51:21 +0900

    add RDBASE to xterm-profile

--- xterm-profile.sh
+++ xterm-profile.sh
@@ -5,6 +5,7 @@ ace_version=6.2.8
 qwt_version=6.1.2-svn
 boost_version=1_56
 xlocal=/nfs/pi-wheezy
+xtools=/nfs/home/rpi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64
 xarch=
 
 if [ $# -ge 1 ]
@@ -14,11 +15,12 @@ then
     case $xarch in
 	pi)
 	    export QTDIR=/opt/qt5-rpi
-	    export BOOST_ROOT=$xlocaL/boost-$boost_version
-	    export ACE_ROOT=$xlocaL/ace+tao/$ace_version
-	    export QWT=$xlocaL/qwt-$qwt_version
+	    export BOOST_ROOT=$xlocal/boost-$boost_version
+	    export ACE_ROOT=$xlocal/ace+tao/$ace_version
+	    export QWT=$xlocal/qwt-$qwt_version
+            export RDBASE=$xlocal
 	    export PATH=$QTDIR/bin:/usr/local/ace+tao/$ace_version/bin:$PATH
-	    export PATH=$PATH:/nfs/home/rpi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin
+	    export PATH=$PATH:$xtools/bin
 	    export XMLPATTERNS=/opt/Qt5.3.2/5.3/gcc_64/bin/xmlpatterns
 	    xterm -bg firebrick -fg floralwhite -T "`uname -n` for $xarch target" &
 	    ;;
@@ -31,6 +33,7 @@ else
 	    export BOOST_ROOT=/usr/local/boost-$boost_version
 	    export ACE_ROOT=/usr/local/ace+tao/$ace_version
 	    export QWT=/usr/local/qwt-$qwt_version
+	    export RDBASE=/usr/local
 	    export PATH=$QTDIR/bin:/usr/local/ace+tao/$ace_version/bin:$PATH
 	    xterm -bg floralwhite &
 	    ;;
@@ -40,6 +43,7 @@ else
 	    export BOOST_ROOT=/usr/local/boost-$boost_version
 	    export ACE_ROOT=/usr/local/ace+tao/$ace_version
 	    export QWT=/usr/local/qwt-$qwt_version
+	    export RDBASE=/usr/local
 	    export PATH=$QTDIR/bin:$ACE_ROOT/bin:$PATH
 	    xterm -bg ivory &
 	    ;;
@@ -49,6 +53,7 @@ else
 	    export BOOST_ROOT=/usr/local/boost-$boost_version
 	    export ACE_ROOT=/usr/local/ace+tao/$ace_version
 	    export QWT=/usr/local/qwt-$qwt_version
+	    export RDBASE=/usr/local
 	    export PATH=$QTDIR/bin:$ACE_ROOT/bin:$PATH
 	    xterm -bg lavender &
 	    ;;
