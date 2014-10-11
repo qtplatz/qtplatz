7ab62a39737ded100c0950f2d160f4bfa03fea7e
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2014-10-11 15:27:35 +0900

    add boost/archive/detail/common_[io]archive.hpp to workaround

---
+++ src/libs/workaround/boost/archive/detail/common_iarchive.hpp
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#include <compiler/diagnostic_push.h>
+#include <compiler/disable_unused_parameter.h>
+
+#include <boost/archive/detail/common_iarchive.hpp>
+
+#include <compiler/diagnostic_pop.h>
+
+
