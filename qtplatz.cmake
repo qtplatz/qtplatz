8e287d9e1b1f97e71e0283f9acff1e0d30b45398
Toshi Hondo, Ph.D <toshi.hondo@qtplatz.com>
2015-01-12 14:17:19 +0900

    Replace FindRDKit instead FindRDKitConfig

--- qtplatz.cmake
+++ qtplatz.cmake
@@ -2,42 +2,22 @@
 
 set( Boost_USE_STATIC_LIBS ON )
 set( BOOST_VERSION boost-1_57 )
-set( QWT_DIR $ENV{QWT} )
 
-include(CheckSymbolExists)
+find_package( arch )
 
-if(WIN32)
+#####################
+# boost setup
+#
+if( WIN32 )
 
-  check_symbol_exists( "_M_AMD64" "" RTC_ARCH_X64 )
-
-  if ( NOT RTC_ARCH_X64 )
-    check_symbol_exists( "_M_IX86" "" RTC_ARCH_X86 )
-  endif( NOT RTC_ARCH_X64 )
-
-  check_symbol_exists( "__i386__" "" RTC_ARCH_X86 )
-  check_symbol_exists( "__x86_64__" "" RTC_ARCH_X64 )
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_WIN32_WINNT=0x0601")
-  set(Boost_INCLUDE_DIR "C:/Boost/include/${BOOST_VERSION}")
+  set( BOOST_ROOT "C:/Boost/include/${BOOST_VERSION}" )
 
   if ( RTC_ARCH_X64 )
-    set( Boost_LIBRARY_DIR "C:/Boost/x86_64/lib")
-    set( CMAKE_PREFIX_PATH "C:/Qt/5.4/msvc2013_64_opengl" )
-    message(STATUS "Building for x86_64 64bit target")
-  endif( RTC_ARCH_X64 )
-
-  if ( RTC_ARCH_X86 )
-    set( Boost_LIBRARY_DIR "C:/Boost/lib")
-    set( CMAKE_PREFIX_PATH "C:/Qt/5.4/msvc2013_opengl" )
-    message(STATUS "Building for x86 32bit target")
-  endif( RTC_ARCH_X86 )
-
-  if ( QWT_DIR STREQUAL "" )
-    set( QWT_DIR "C:\Qwt-6.1.3-svn" )
+    set( Boost_LIBRARY_DIR "C:/Boost/x86_64/lib" )
   endif()
 
-else( WIN32 )
+else()
 
-  check_symbol_exists( "__arm__" "" RTC_ARCH_ARM )
   if ( RTC_ARCH_ARM )
     set(Boost_INCLUDE_DIR "/usr/local/arm-linux-gnueabihf/usr/local/include")
     set(Boost_LIBRARY_DIR "/usr/local/arm-linux-gnueabihf/usr/local/lib")
@@ -47,19 +27,23 @@ else( WIN32 )
   endif()
 
 endif()
+  
+#find_package(Boost REQUIRED)
 
-find_package(Boost REQUIRED)
-
-add_definitions(-DUNICODE -D_UNICODE)
-
+#####################
+# Compiler setup
+#
 if (MSVC)
 
+  set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_WIN32_WINNT=0x0601")
+  add_definitions(-DUNICODE -D_UNICODE)
   message(STATUS "Using ${CMAKE_CXX_COMPILER}. C++11 support is native.")
 
 else()
 
   include(CheckCXXCompilerFlag)
   CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+  CHECK_CXX_COMPILER_FLAG("-std=c++03" COMPILER_SUPPORTS_CXX03)
 
   if(COMPILER_SUPPORTS_CXX11)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
@@ -76,10 +60,5 @@ else()
 
 endif()
 
-set(CMAKE_INSTALL_PREFIX ./bin)
-set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
-set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
-
-include_directories(${Boost_INCLUDE_DIRS} ${CMAKE_CURRENT_SOURCE_DIR}/src/libs )
-link_directories(${Boost_LIBRARY_DIRS} ${CMAKE_BINARY_DIR}/lib)
+#include_directories(${Boost_INCLUDE_DIRS} ${CMAKE_CURRENT_SOURCE_DIR}/src/libs )
+#link_directories(${Boost_LIBRARY_DIRS} ${CMAKE_BINARY_DIR}/lib)
