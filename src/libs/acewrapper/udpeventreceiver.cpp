fc6151b21fd23766a237a924ba079bf99f8d4288
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2015-02-07 17:40:20 +0900

    fix qtplatz corelib build on linux

--- src/libs/acewrapper/udpeventreceiver.cpp
+++ src/libs/acewrapper/udpeventreceiver.cpp
@@ -22,8 +22,6 @@
 **
 **************************************************************************/
 
-#pragma once
-
 #include "udpeventreceiver.hpp"
 #include <workaround/boost/asio.hpp>
 #include <vector>
