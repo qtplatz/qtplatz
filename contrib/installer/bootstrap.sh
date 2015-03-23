f5ad2447879d03e28cad82956abe5fdf04c57c9c
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2015-03-23 14:14:25 +0900

    improve bootstrap.sh for installer

--- contrib/installer/bootstrap.sh
+++ contrib/installer/bootstrap.sh
@@ -5,32 +5,32 @@ if [ -z $cross_target ]; then
     exit 3
 fi
 
+args="$@"
+
 if [ $# -eq 0 ]; then
-    echo "No arguments provided"
-    echo $0 'qt5 | boost | ace+tao'
-    exit 3
+    args="boost qt5 ace+tao"
 fi
 
-for var in "$@"
+for var in $args
 do
     case $var in
-	qt5)
+	qt5|boost|ace+tao)
 	    echo "building package generator for $var on $cross_target"
 	    mkdir -p build-$cross_target/$var
-	    cd build-$cross_target/$var
-	    cmake -DCMAKE_TOOLCHAIN_FILE=../../../../toolchain-$cross_target.cmake ../../$var
+	    ( cd build-$cross_target/$var;
+	      cmake -DCMAKE_TOOLCHAIN_FILE=../../../../toolchain-$cross_target.cmake ../../$var )
 	    ;;
-	boost)
-	    echo "building package generator for $var on $cross_target"
-	    mkdir -p build-$cross_target/$var
-	    cd build-$cross_target/$var
-	    cmake -DCMAKE_TOOLCHAIN_FILE=../../../../toolchain-$cross_target.cmake ../../$var
+	clean)
+	    echo rm -rf build-$cross_target
+	    rm -rf build-$cross_target
 	    ;;
-	ace+tao)
-	    echo "building package generator for $var on $cross_target"
-	    mkdir -p build-$cross_target/$var
-	    cd build-$cross_target/$var
-	    cmake -DCMAKE_TOOLCHAIN_FILE=../../../../toolchain-$cross_target.cmake ../../$var
+	build)
+	    for pkg in boost qt5 ace+tao; do
+		( cd build-$cross_target/$pkg;
+		  make package;
+		  mv *.deb ..
+		)
+	    done
 	    ;;
 	*)
 	    echo "unknown target: $var"
