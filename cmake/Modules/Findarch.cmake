e65dd03c2c4371d71a2676d091d7987731ea0f07
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2015-03-24 11:09:18 +0900

    fix Findarch.cmake on linux 64bit detetion

--- cmake/Modules/Findarch.cmake
+++ cmake/Modules/Findarch.cmake
@@ -1,32 +1,49 @@
 #find target arch
 
-if ( NOT arch_FOUND )
+include(CheckSymbolExists)
 
-  include(CheckSymbolExists)
+if ( arch_FOUND )
+  return()
+endif()
 
-  if( WIN32 )
-    
-    check_symbol_exists( "_M_AMD64" "" RTC_ARCH_X64 )
-    
-    if ( NOT ARCH_X64 )
-      check_symbol_exists( "_M_IX86" "" RTC_ARCH_X86 )
-    endif()
-    
+if( WIN32 )
+  
+  check_symbol_exists( "_M_AMD64" "" RTC_ARCH_X64 )
+  
+  if ( NOT RTC_ARCH_X64 )
+    check_symbol_exists( "_M_X64" "" RTC_ARCH_X64 )
+  endif()
+
+  if ( NOT RTC_ARCH_X64 )
+    check_symbol_exists( "_M_IX86" "" RTC_ARCH_X86 )
+  endif()
+
+else()  
+
+  check_symbol_exists( "__x86_64__" "" RTC_ARCH_X64 )
+
+  if ( NOT RTC_ARCH_X64 )  
     check_symbol_exists( "__i386__" "" RTC_ARCH_X86 )
-    check_symbol_exists( "__x86_64__" "" RTC_ARCH_X64 )
-    
-  else()
-    
-    check_symbol_exists( "__arm__" "" RTC_ARCH_ARM )
-    
-  endif()
 
-  if ( RTC_ARCH_X64 )
-    set( arch_FOUND "x86_64" )
-  elseif( RTC_ARCH_X86 )
-    set( arch_FOUND "x86" )
-  elseif( RTC_ARCH_ARM )
-    set( arch_FOUND "arm" )
+    if ( NOT RTC_ARCH_X86 )
+      check_symbol_exists( "__arm__" "" RTC_ARCH_ARM )      
+    endif()
+
   endif()
+  
+endif()
+
+if ( RTC_ARCH_X64 )
+
+  set( arch_FOUND "x86_64" )
+
+elseif( RTC_ARCH_X86 )
+
+  set( arch_FOUND "x86" )
+
+elseif( RTC_ARCH_ARM )
+
+  set( arch_FOUND "arm" )
 
 endif()
+
