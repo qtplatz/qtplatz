f524551b5b42e10cd6d4ee6f147ccdc924972de8
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2015-04-20 09:37:06 +0900

    cpack script for raspi -- briefly tested

--- cmake/Modules/qt5-cpack.cmake
+++ cmake/Modules/qt5-cpack.cmake
@@ -1,57 +1,61 @@
 
-if ( Qt5_FOUND )
+if ( WIN32 )
+  set( SO "dll")
+elseif( APPLE )
+  set( SO "dylib" )
+else()
+  set( SO "so" )
+endif()
 
-  find_package( Qt5 REQUIRED Core Gui Help Multimedia Network OpenGL PrintSupport Script Sql Svg Widgets Xml XmlPatterns )
 
-  foreach(plugin ${Qt5Gui_PLUGINS} ${Qt5Svg_PLUGINS} ${Qt5Sql_PLUGINS} )
-    get_target_property( _loc ${plugin} LOCATION )
-    file( RELATIVE_PATH _rname $ENV{QTDIR}/plugins ${_loc} )
-    get_filename_component(_rpath ${_rname} DIRECTORY )
-    install( FILES ${_loc} DESTINATION plugins/${_rpath} COMPONENT runtime_libraries )
-  endforeach()
+find_package( Qt5 REQUIRED Core Gui Multimedia Network OpenGL PrintSupport Script Sql Svg Widgets Xml XmlPatterns )
 
-  if ( WIN32 )
-    set( SO "dll")
-  elseif( APPLE )
-    set( SO "dylib" )
-  else()
-    set( SO "so" )
-  endif()
-
-  get_target_property( _loc Qt5::Core LOCATION )
-
-  get_filename_component( _dir ${_loc} DIRECTORY )
-
-  # message( "qt5 dir: " ${_dir}/icuin52.${SO} )
-
-  install( FILES ${_dir}/qt.conf DESTINATION bin COMPONENT runtime_libraries )
+if ( NOT CMAKE_CROSSCOMPILING )
+  find_package( Qt5 REQUIRED Help )
+endif()
   
+foreach(plugin ${Qt5Gui_PLUGINS} ${Qt5Svg_PLUGINS} ${Qt5Sql_PLUGINS} )
+  get_target_property( _loc ${plugin} LOCATION )
+  file( RELATIVE_PATH _rname $ENV{QTDIR}/plugins ${_loc} )
+  get_filename_component(_rpath ${_rname} DIRECTORY )
+  install( FILES ${_loc} DESTINATION plugins/${_rpath} COMPONENT runtime_libraries )
+endforeach()
+
+get_target_property( _loc Qt5::Core LOCATION )
+
+get_filename_component( _dir ${_loc} DIRECTORY )
+
+install( FILES ${_dir}/qt.conf DESTINATION bin COMPONENT runtime_libraries )
+
+if ( WIN32 )
   install( FILES
     ${_dir}/icudt53.${SO}
     ${_dir}/icuin53.${SO}
     ${_dir}/icuuc53.${SO}
     ${_dir}/Qt5CLucene.${SO}
     DESTINATION bin COMPONENT runtime_libraries )
+endif()
+
+foreach( lib
+    Qt5::Core
+    Qt5::Gui
+    Qt5::Multimedia
+    Qt5::Network
+    Qt5::OpenGL
+    Qt5::PrintSupport
+    Qt5::Script
+    Qt5::Sql
+    Qt5::Svg
+    Qt5::Widgets
+    Qt5::Xml
+    Qt5::XmlPatterns )
   
-  foreach( lib
-      Qt5::Core
-      Qt5::Gui
-      Qt5::Help
-      Qt5::Multimedia
-      Qt5::Network
-      Qt5::OpenGL
-      Qt5::PrintSupport
-      Qt5::Script
-      Qt5::Sql
-      Qt5::Svg
-      Qt5::Widgets
-      Qt5::Xml
-      Qt5::XmlPatterns )
+  get_target_property( _loc ${lib} LOCATION )
+  install( FILES ${_loc} DESTINATION bin COMPONENT runtime_libraries )
+  
+endforeach()
 
-    get_target_property( _loc ${lib} LOCATION )
-    # message( "##### Qt5 : " ${_loc} )
-    install( FILES ${_loc} DESTINATION bin COMPONENT runtime_libraries )
-
-  endforeach()
-
+if ( NOT CMAKE_CROSSCOMPILING )
+  get_target_property( _loc Qt5::Help LOCATION )
+  install( FILES ${_loc} DESTINATION bin COMPONENT runtime_libraries )
 endif()
