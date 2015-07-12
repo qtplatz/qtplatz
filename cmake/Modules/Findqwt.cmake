185c35828cf335dcf7b16bb3bcf814f5639db660
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2015-07-12 17:39:26 +0900

    apply workaround for helio build

--- cmake/Modules/Findqwt.cmake
+++ cmake/Modules/Findqwt.cmake
@@ -5,7 +5,7 @@ if ( qwt_FOUND )
 endif()
 
 if ( WIN32 )
-  find_path( qwt_DIR NAMES include/qwt.h HINTS $ENV{QWT} C:/Qwt-6.1.2-svn C:/Qwt-6.1.3-svn )
+  find_path( qwt_DIR NAMES include/qwt.h HINTS $ENV{QWT} C:/Qwt-6.1.3-svn C:/Qwt-6.1.2-svn )
 else()
   find_path( qwt_DIR NAMES include/qwt.h HINTS $ENV{QWT} /usr/local/qwt-6.1.3-svn /usr/local/qwt-6.1.2-svn )
 endif()
@@ -15,12 +15,15 @@ if ( qwt_DIR )
   set( QWT_INCLUDE_DIR ${qwt_DIR}/include )
   set( QWT_INCLUDE_DIRS ${QWT_INCLUDE_DIR} )
 
-  include( qwt-config )
-  set( QWT_LIBRARIES qwt )
+  find_library( QWT_LIBRARIES NAMES qwt HINTS ${qwt_DIR}/lib )
 
-  set( qwt_FOUND 1 )
+  if ( QWT_LIBRARIES )
+  
+    set( qwt_FOUND 1 )
+    message( STATUS "qwt_DIR : " ${qwt_DIR} )
+    message( STATUS "QWT_LIBRARIES : " ${QWT_LIBRARIES} )
 
-  message( STATUS "QWT Found in : " ${qwt_DIR} )
+  endif()
 
 else()
 
