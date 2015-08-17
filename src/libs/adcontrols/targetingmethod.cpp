65fb823d2a6edfe3cb057605a635c2e641a0e558
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2015-08-17 14:24:09 +0900

    targeting method transition to moltable

--- src/libs/adcontrols/targetingmethod.cpp
+++ src/libs/adcontrols/targetingmethod.cpp
@@ -22,12 +22,150 @@
 **
 **************************************************************************/
 
-#include <compiler/disable_unused_parameter.h>
 #include "targetingmethod.hpp"
+#include "serializer.hpp"
+#include "moltable.hpp"
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/version.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/utility.hpp>
+
+namespace adcontrols {
+
+    class TargetingMethod::impl {
+    public:
+        impl( idTarget id ) : idTarget_( id )
+            , toleranceMethod_( idToleranceDaltons )
+            , tolerancePpm_( 10.0 )
+            , toleranceDaltons_( 0.010 ) // 10mDa
+            , chargeStateMin_( 1 )
+            , chargeStateMax_( 3 )
+            , isLowMassLimitEnabled_( false ) // auto
+            , isHighMassLimitEnabled_( false )
+            , lowMassLimit_( 1 )
+            , highMassLimit_( 1000 )
+            , tolerance_( 10.0 ) { // deplicated
+            // reference,
+            // http://fiehnlab.ucdavis.edu/staff/kind/Metabolomics/MS-Adduct-Calculator/
+            for ( auto adduct : { "H", "Na", "NH4", "K", "CH3CN+H" "CH3CN+Na", "CH3OH+H", "(CH3)2SO+H", "C3H8O+H", "C3H8O+Na" } )
+                pos_adducts_.push_back( std::make_pair( false, adduct ) );
+
+
+            for ( auto adduct : { "-H", "-H2O-H", "Na-H2", "Cl", "K-H2", "COOH-H" } )
+                neg_adducts_.push_back( std::make_pair( false, adduct ) );
+        }
+        
+        idTarget idTarget_;
+        idToleranceMethod toleranceMethod_;
+        double tolerancePpm_;
+        double toleranceDaltons_;
+        uint32_t chargeStateMin_;
+        uint32_t chargeStateMax_;
+        bool isLowMassLimitEnabled_;
+        bool isHighMassLimitEnabled_;
+        double lowMassLimit_;
+        double highMassLimit_;
+        double tolerance_;
+
+        std::vector< std::pair< bool, std::string > > pos_adducts_; // if start with '-' means lose instead of add
+        std::vector< std::pair< bool, std::string > > neg_adducts_;
+
+        std::vector< formula_type > formulae_;
+        std::vector< peptide_type > peptides_;
+        moltable molecules_;
+        //----------
+        
+        friend class boost::serialization::access;
+        template<class Archive> void serialize(Archive& ar, const unsigned int version ) {
+            using namespace boost::serialization;
+
+            bool is_use_resolving_power; // workaround
+
+            if ( version <= 1 ) {
+                std::vector< std::pair< std::wstring, bool > > adducts;
+                std::vector< std::pair< std::wstring, bool > > formulae;
+                bool isPositive;
+
+                ar & BOOST_SERIALIZATION_NVP( isPositive );
+                ar & BOOST_SERIALIZATION_NVP( is_use_resolving_power );
+                ar & BOOST_SERIALIZATION_NVP( tolerancePpm_ );
+                ar & BOOST_SERIALIZATION_NVP( toleranceDaltons_ );
+                ar & BOOST_SERIALIZATION_NVP( chargeStateMin_ );
+                ar & BOOST_SERIALIZATION_NVP( chargeStateMax_ );
+                ar & BOOST_SERIALIZATION_NVP( isLowMassLimitEnabled_ );
+                ar & BOOST_SERIALIZATION_NVP( isHighMassLimitEnabled_ );
+                ar & BOOST_SERIALIZATION_NVP( lowMassLimit_ );
+                ar & BOOST_SERIALIZATION_NVP( highMassLimit_ );
+                ar & BOOST_SERIALIZATION_NVP( tolerance_ );
+                ar & BOOST_SERIALIZATION_NVP( formulae );
+                ar & BOOST_SERIALIZATION_NVP( adducts ); // pos
+                ar & BOOST_SERIALIZATION_NVP( adducts ); // neg
+            } else {
+                ar & BOOST_SERIALIZATION_NVP( idTarget_ )
+                    ;
+                if ( version < 4 )
+                    ar & BOOST_SERIALIZATION_NVP( is_use_resolving_power );
+                else if ( version >= 4 )
+                    ar & BOOST_SERIALIZATION_NVP( toleranceMethod_ );
+                ar & BOOST_SERIALIZATION_NVP( tolerancePpm_ )
+                    & BOOST_SERIALIZATION_NVP( toleranceDaltons_ )
+                    & BOOST_SERIALIZATION_NVP( chargeStateMin_ )
+                    & BOOST_SERIALIZATION_NVP( chargeStateMax_ )
+                    & BOOST_SERIALIZATION_NVP( isLowMassLimitEnabled_ )
+                    & BOOST_SERIALIZATION_NVP( isHighMassLimitEnabled_ )
+                    & BOOST_SERIALIZATION_NVP( lowMassLimit_ )
+                    & BOOST_SERIALIZATION_NVP( highMassLimit_ )
+                    & BOOST_SERIALIZATION_NVP( tolerance_ )
+                    & BOOST_SERIALIZATION_NVP( pos_adducts_ )
+                    & BOOST_SERIALIZATION_NVP( neg_adducts_ )
+                    ;
+                if ( version < 3 ) {
+                    std::vector< std::pair< bool, std::string > > formulae;
+                    std::vector< std::pair< bool, std::pair< std::string, std::string > > > peptides;
+                    ar & BOOST_SERIALIZATION_NVP( formulae )
+                        & BOOST_SERIALIZATION_NVP( peptides )
+                        ;
+                } else {
+                    ar & BOOST_SERIALIZATION_NVP( formulae_ )
+                        & BOOST_SERIALIZATION_NVP( peptides_ )
+                        ;
+                }
+            }
+        }
+    };
+    ////////// PORTABLE BINARY ARCHIVE //////////
+    template<> void
+    TargetingMethod::serialize( portable_binary_oarchive& ar, const unsigned int )
+    {
+        ar & *impl_;
+    }
+
+    template<> void
+    TargetingMethod::serialize( portable_binary_iarchive& ar, const unsigned int version )
+    {
+        impl_->serialize( ar, version );
+    }
+
+    ///////// XML archive ////////
+    template<> void
+    TargetingMethod::serialize( boost::archive::xml_woarchive& ar, const unsigned int )
+    {
+        ar & boost::serialization::make_nvp("impl", *impl_);
+    }
+
+    template<> void
+    TargetingMethod::serialize( boost::archive::xml_wiarchive& ar, const unsigned int version )
+    {
+        impl_->serialize( ar, version );
+    }
+}
+
 
 using namespace adcontrols;
 
-TargetingMethod::TargetingMethod( idTarget id ) : idTarget_( id )
+TargetingMethod::TargetingMethod( idTarget id ) : impl_( new impl( id ) )
+                                                , idTarget_( id )
                                                 , toleranceMethod_( idToleranceDaltons )
                                                 , tolerancePpm_( 10.0 )
                                                 , toleranceDaltons_( 0.010 ) // 10mDa
@@ -37,7 +175,7 @@ TargetingMethod::TargetingMethod( idTarget id ) : idTarget_( id )
                                                 , isHighMassLimitEnabled_( false )
                                                 , lowMassLimit_( 1 )
                                                 , highMassLimit_( 1000 )
-    , tolerance_( 10.0 ) // deplicated
+                                                , tolerance_( 10.0 ) // deplicated
 {
     // reference, 
     // http://fiehnlab.ucdavis.edu/staff/kind/Metabolomics/MS-Adduct-Calculator/
--- src/libs/adcontrols/targetingmethod.hpp
+++ src/libs/adcontrols/targetingmethod.hpp
@@ -28,12 +28,8 @@
 
 #include "adcontrols_global.h"
 #include "msfinder.hpp"
-#include <boost/serialization/nvp.hpp>
 #include <boost/serialization/version.hpp>
-#include <boost/serialization/string.hpp>
-#include <boost/serialization/vector.hpp>
-#include <boost/serialization/utility.hpp>
-
+#include <cstdint>
 #include <string>
 #include <vector>
 #include <compiler/disable_dll_interface.h>
@@ -42,6 +38,8 @@ namespace boost { namespace serialization {  class access;  } }
 
 namespace adcontrols {
 
+    class moltable;
+    
     class ADCONTROLSSHARED_EXPORT TargetingMethod {
     public:
         enum idTarget { idTargetFormula, idTargetPeptide };
@@ -83,8 +81,9 @@ namespace adcontrols {
         double highMassLimit() const;
         void highMassLimit( double );
 
-        //double tolerance() const;
-        //void tolerance( double );
+        const moltable& molecules() const;
+        moltable& molecules();
+        void set_molecules( const moltable& );
 
         struct ADCONTROLSSHARED_EXPORT formula_data {
             TargetingMethod::formula_type f_;
@@ -98,6 +97,9 @@ namespace adcontrols {
         };
 
     private:
+        class impl;
+        impl * impl_;
+        
         idTarget idTarget_;
         idToleranceMethod toleranceMethod_;
         double tolerancePpm_;
@@ -115,64 +117,9 @@ namespace adcontrols {
 
         std::vector< formula_type > formulae_;
         std::vector< peptide_type > peptides_;
-        
+
         friend class boost::serialization::access;
-        template<class Archive> void serialize(Archive& ar, const unsigned int version ) {
-            using namespace boost::serialization;
-
-            bool is_use_resolving_power; // workaround
-
-            if ( version <= 1 ) {
-                std::vector< std::pair< std::wstring, bool > > adducts;
-                std::vector< std::pair< std::wstring, bool > > formulae;
-                bool isPositive;
-
-                ar & BOOST_SERIALIZATION_NVP( isPositive );
-                ar & BOOST_SERIALIZATION_NVP( is_use_resolving_power );
-                ar & BOOST_SERIALIZATION_NVP( tolerancePpm_ );
-                ar & BOOST_SERIALIZATION_NVP( toleranceDaltons_ );
-                ar & BOOST_SERIALIZATION_NVP( chargeStateMin_ );
-                ar & BOOST_SERIALIZATION_NVP( chargeStateMax_ );
-                ar & BOOST_SERIALIZATION_NVP( isLowMassLimitEnabled_ );
-                ar & BOOST_SERIALIZATION_NVP( isHighMassLimitEnabled_ );
-                ar & BOOST_SERIALIZATION_NVP( lowMassLimit_ );
-                ar & BOOST_SERIALIZATION_NVP( highMassLimit_ );
-                ar & BOOST_SERIALIZATION_NVP( tolerance_ );
-                ar & BOOST_SERIALIZATION_NVP( formulae );
-                ar & BOOST_SERIALIZATION_NVP( adducts ); // pos
-                ar & BOOST_SERIALIZATION_NVP( adducts ); // neg
-            } else {
-                ar & BOOST_SERIALIZATION_NVP( idTarget_ )
-                    ;
-                if ( version < 4 )
-                    ar & BOOST_SERIALIZATION_NVP( is_use_resolving_power );
-                else if ( version >= 4 )
-                    ar & BOOST_SERIALIZATION_NVP( toleranceMethod_ );
-                ar & BOOST_SERIALIZATION_NVP( tolerancePpm_ )
-                    & BOOST_SERIALIZATION_NVP( toleranceDaltons_ )
-                    & BOOST_SERIALIZATION_NVP( chargeStateMin_ )
-                    & BOOST_SERIALIZATION_NVP( chargeStateMax_ )
-                    & BOOST_SERIALIZATION_NVP( isLowMassLimitEnabled_ )
-                    & BOOST_SERIALIZATION_NVP( isHighMassLimitEnabled_ )
-                    & BOOST_SERIALIZATION_NVP( lowMassLimit_ )
-                    & BOOST_SERIALIZATION_NVP( highMassLimit_ )
-                    & BOOST_SERIALIZATION_NVP( tolerance_ )
-                    & BOOST_SERIALIZATION_NVP( pos_adducts_ )
-                    & BOOST_SERIALIZATION_NVP( neg_adducts_ )
-                    ;
-                if ( version < 3 ) {
-                    std::vector< std::pair< bool, std::string > > formulae;
-                    std::vector< std::pair< bool, std::pair< std::string, std::string > > > peptides;
-                    ar & BOOST_SERIALIZATION_NVP( formulae )
-                        & BOOST_SERIALIZATION_NVP( peptides )
-                        ;
-                } else {
-                    ar & BOOST_SERIALIZATION_NVP( formulae_ )
-                        & BOOST_SERIALIZATION_NVP( peptides_ )
-                        ;
-                }
-            }
-        }
+        template<class Archive> void serialize(Archive& ar, const unsigned int version );
     };
 
 }
