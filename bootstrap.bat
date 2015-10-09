1558206889fb8439174feba29d0678134cc08b67
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2015-10-10 08:16:33 +0900

    experimental add for vc14 support - not working yet

--- bootstrap.bat
+++ bootstrap.bat
@@ -5,6 +5,7 @@ set build_root=..\build-x86_64
 set build_type=debug
 set build_tests=false
 set build_clean=false
+set tools=vc12
 
 for %%i in (%*) do (
     if %%i==release (
@@ -15,10 +16,12 @@ for %%i in (%*) do (
        set build_tests=true
     ) else if %%i==clean (
        set build_clean=true
+    ) else if %%i==vc14 (
+       set tools=vc14
     )
 )
 
-set build_dir=%build_root%\qtplatz.%build_type%
+set build_dir=%build_root%\qtplatz.%build_type%-%tools%
 
 if %build_clean%==true (
   echo rmdir %build_dir% /s /q
@@ -29,22 +32,38 @@ if %build_clean%==true (
 mkdir %build_dir%
 cd %build_dir%
 
-if %build_type%==release (
-  cmake -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=ON %source_dir%
-) else if %build_type%==package (
-  cmake -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=OFF %source_dir%
-  cd contrib\installer\wix
-  nmake help
-) else (
-  cmake -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Debug %source_dir%
+if %tools%==vc12 (
+
+   if %build_type%==release (
+     cmake -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=ON %source_dir%
+   ) else if %build_type%==package (
+     cmake -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=OFF %source_dir%
+     cd contrib\installer\wix
+     nmake help
+     got end
+   ) else (
+     cmake -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Debug %source_dir%
+   )
+   if %build_tests%==true (
+      echo "Build tests directory on %cd%"
+      mkdir tests
+      cd tests
+      cmake -G "Visual Studio 12 Win64" %source_dir%\tests
+   )
+   
+) else if %tools%==vc14 (
+
+  if %build_type%==release (
+     cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=ON %source_dir%
+  ) else if %build_type%==package (
+     cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=OFF %source_dir%
+     cd contrib\installer\wix
+     nmake help
+  ) else (
+     cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Debug %source_dir%
+  )
 )
 
-if %build_tests%==true (
-   echo "Build tests directory on %cd%"
-   mkdir tests
-   cd tests
-   cmake -G "Visual Studio 12 Win64" %source_dir%\tests
-   cd ..
-)
+cd %source_dir%
 
 :end
