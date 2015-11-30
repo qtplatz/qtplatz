3117f805b0c655236b84788bbebb7f1cb702e443
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2015-11-30 16:58:22 +0900

    add sequence que

---
+++ src/libs/adicontroller/sequence.cpp
@@ -0,0 +1,113 @@
+// -*- C++ -*-
+/**************************************************************************
+** Copyright (C) 2010-2016 Toshinobu Hondo, Ph.D.
+** Copyright (C) 2013-2016 MS-Cheminformatics LLC
+*
+** Contact: info@ms-cheminfo.com
+**
+** Commercial Usage
+**
+** Licensees holding valid MS-Cheminformatics commercial licenses may use this
+** file in accordance with the MS-Cheminformatics Commercial License Agreement
+** provided with the Software or, alternatively, in accordance with the terms
+** contained in a written agreement between you and MS-Cheminformatics.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.TXT included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
+#include "sequence.hpp"
+#include "sampleprocessor.hpp"
+#include <deque>
+#include <mutex>
+#include <thread>
+
+namespace adicontroller {
+
+    class sequence::impl {
+    public:
+        static std::unique_ptr< sequence > instance_;
+        std::mutex mutex_;
+        
+        std::deque< sequence::value_type > que_; // SampleProcessors
+    };
+
+    std::unique_ptr< sequence > sequence::impl::instance_;    
+}
+
+using namespace adicontroller;
+
+sequence::~sequence()
+{
+}
+
+sequence::sequence() : impl_( new impl() )
+{
+}
+
+sequence *
+sequence::instance()
+{
+    static std::once_flag flag;
+    std::call_once( flag, [](){ sequence::impl::instance_.reset( new sequence() ); } );
+
+    return sequence::impl::instance_.get();
+}
+
+sequence::iterator
+sequence::begin()
+{
+    return impl_->que_.begin();
+}
+
+sequence::iterator
+sequence::end()
+{
+    return impl_->que_.end();    
+}
+
+sequence::const_iterator
+sequence::begin() const
+{
+    return impl_->que_.begin();
+}
+
+sequence::const_iterator
+sequence::end() const
+{
+    return impl_->que_.end();        
+}
+
+size_t
+sequence::size() const
+{
+    return impl_->que_.size();
+}
+
+void
+sequence::post( value_type sp )
+{
+    std::lock_guard< std::mutex > lock( impl_->mutex_ );
+    
+    impl_->que_.push_back( sp );
+}
+
+sequence::value_type
+sequence::deque()
+{
+    std::lock_guard< std::mutex > lock( impl_->mutex_ );
+    
+    if ( !impl_->que_.empty() ) {
+        auto sp = impl_->que_.front();
+        impl_->que_.pop_front();
+        return sp;
+    }
+    return 0;
+}
---
+++ src/libs/adicontroller/sequence.hpp
@@ -0,0 +1,72 @@
+// -*- C++ -*-
+/**************************************************************************
+** Copyright (C) 2010-2016 Toshinobu Hondo, Ph.D.
+** Copyright (C) 2013-2016 MS-Cheminformatics LLC
+*
+** Contact: info@ms-cheminfo.com
+**
+** Commercial Usage
+**
+** Licensees holding valid MS-Cheminformatics commercial licenses may use this
+** file in accordance with the MS-Cheminformatics Commercial License Agreement
+** provided with the Software or, alternatively, in accordance with the terms
+** contained in a written agreement between you and MS-Cheminformatics.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.TXT included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
+#pragma once
+
+#include <deque>
+#include <memory>
+
+namespace adicontroller {
+
+    class SampleProcessor;
+
+    namespace internal {  struct receiver_data;  }
+
+    class sequence {
+
+        sequence( const sequence& ) = delete;
+        sequence& operator = ( const sequence& ) = delete;
+        
+        ~sequence();
+        sequence();
+
+    public:
+        static sequence * instance();
+
+        typedef std::shared_ptr< SampleProcessor > value_type;
+        typedef std::shared_ptr< const SampleProcessor > const_value_type;
+
+        typedef std::deque< value_type >::iterator iterator;
+        typedef std::deque< value_type >::const_iterator const_iterator;
+
+        iterator begin();
+        iterator end();
+        const_iterator begin() const;
+        const_iterator end() const;
+
+        size_t size() const;
+
+        void post( value_type ); // <-- preparing_for_run
+
+        value_type deque();      // <-- stop run | reset
+
+    private:
+        friend std::unique_ptr< sequence >::deleter_type;
+        class impl;
+        std::unique_ptr< impl > impl_;
+    };
+
+} // namespace adicontroller
+
