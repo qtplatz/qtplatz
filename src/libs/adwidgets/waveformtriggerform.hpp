a403ab7cac40744e4b69d99cd88970c4d2c984fd
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2016-01-05 19:52:31 +0900

    change LifeCycle::setContents rvalue reference

--- src/libs/adwidgets/waveformtriggerform.hpp
+++ src/libs/adwidgets/waveformtriggerform.hpp
@@ -50,7 +50,7 @@ namespace adwidgets {
         void OnInitialUpdate() override;
         void OnFinalClose() override;
         bool getContents( boost::any& ) const override;
-        bool setContents( boost::any& ) override;
+        bool setContents( boost::any&& ) override;
         void * query_interface_workaround( const char * typenam ) override;
 
     private:
