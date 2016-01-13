8addab25bca408f1da152f7fb8c313334ee38bbc
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2016-01-13 20:27:14 +0900

    Add TofProtocol in adcontrols

---
+++ src/libs/adcontrols/tofprotocol.cpp
@@ -0,0 +1,161 @@
+/**************************************************************************
+** Copyright (C) 2010-2015 Toshinobu Hondo, Ph.D.
+** Copyright (C) 2013-2015 MS-Cheminformatics LLC
+*
+** Contact: toshi.hondo@qtplatz.com or info@ms-cheminfo.com
+**
+** Commercial Usage
+**
+** Licensees holding valid MS-Cheminformatics commercial licenses may use this file in
+** accordance with the MS-Cheminformatics Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and MS-Cheminformatics.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.TXT included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
+#include "tofprotocol.hpp"
+#include <adcontrols/metric/prefix.hpp>
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/variant.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/version.hpp>
+#include <boost/serialization/nvp.hpp>
+#include <adportable/portable_binary_iarchive.hpp>
+#include <adportable/portable_binary_oarchive.hpp>
+#include <boost/archive/xml_woarchive.hpp>
+#include <boost/archive/xml_wiarchive.hpp>
+
+namespace adcontrols {
+
+    //////////////////////////////////////////
+    template<typename T = TofProtocol >
+    class TofProtocol_archive {
+    public:
+        template<class Archive>
+        void serialize( Archive& ar, T& _, const unsigned int version ) {
+            using namespace boost::serialization;
+            ar & BOOST_SERIALIZATION_NVP( _.lower_mass_ );
+            ar & BOOST_SERIALIZATION_NVP( _.upper_mass_ );
+            ar & BOOST_SERIALIZATION_NVP( _.delay_pulses_ );
+            ar & BOOST_SERIALIZATION_NVP( _.additionals_ );
+            ar & BOOST_SERIALIZATION_NVP( _.reference_ );
+            ar & BOOST_SERIALIZATION_NVP( _.formulae_ );            
+            ar & BOOST_SERIALIZATION_NVP( _.description_ );
+        }
+    };
+    
+    template<> ADCONTROLSSHARED_EXPORT void TofProtocol::serialize( boost::archive::xml_woarchive& ar, const unsigned int version )
+    {
+        TofProtocol_archive<>().serialize( ar, *this, version );
+    }
+    template<> ADCONTROLSSHARED_EXPORT void TofProtocol::serialize( boost::archive::xml_wiarchive& ar, const unsigned int version )
+    {
+        TofProtocol_archive<>().serialize( ar, *this, version );
+    }
+    
+    template<> ADCONTROLSSHARED_EXPORT void TofProtocol::serialize( portable_binary_oarchive& ar, const unsigned int version )
+    {
+        TofProtocol_archive<>().serialize( ar, *this, version );
+    }
+    
+    template<> ADCONTROLSSHARED_EXPORT void TofProtocol::serialize( portable_binary_iarchive& ar, const unsigned int version )
+    {
+        TofProtocol_archive<>().serialize( ar, *this, version );
+    }
+
+}        
+
+using namespace adcontrols;
+
+TofProtocol::TofProtocol() : lower_mass_( 0 )
+                           , upper_mass_( 0 )
+                           , delay_pulses_( { { 0,0 }   // push
+                                   , { 0,0 }            // inject
+                                   , { 0,0 }            // exit
+                                   , { 0,0 }, { 0,0 }   // gate0, gate1
+                                   , { 0,0 } }  )       // ext. trig delay (helio -> digitizer trig.)
+{
+}
+
+TofProtocol::TofProtocol( const TofProtocol& t ) : lower_mass_( t.lower_mass_ )
+                                                 , upper_mass_( t.upper_mass_ )
+                                                 , delay_pulses_( t.delay_pulses_ )
+                                                 , additionals_( t.additionals_ )
+                                                 , reference_( t.reference_ )
+                                                 , formulae_( t.formulae_ )
+                                                 , description_( t.description_ )
+{            
+}
+
+std::vector< TofProtocol::delay_pulse_type >&
+TofProtocol::delay_pulses()
+{
+    return delay_pulses_;    
+}
+
+const std::vector< TofProtocol::delay_pulse_type >&
+TofProtocol::delay_pulses() const
+{
+    return delay_pulses_;
+}
+
+void
+TofProtocol::setDescription( const std::string& value )
+{
+    description_ = value;
+}
+
+const std::string&
+TofProtocol::description() const
+{
+    return description_;
+}
+
+std::vector< std::string> &
+TofProtocol::formulae()
+{
+    return formulae_;
+}
+
+const std::vector< std::string> &
+TofProtocol::formulae() const
+{
+    return formulae_;
+}
+
+std::vector< std::pair< int32_t, TofProtocol::additional_value_type > >&
+TofProtocol::additionals()
+{
+    return additionals_;
+}
+
+const std::vector< std::pair< int32_t, TofProtocol::additional_value_type > >&
+TofProtocol::additionals() const
+{
+    return additionals_;
+}
+
+void
+TofProtocol::setReference( uint32_t reference )
+{
+    reference_ = reference;
+}
+
+uint32_t
+TofProtocol::reference() const
+{
+    return reference_;
+}
+
+///////////////////
+
---
+++ src/libs/adcontrols/tofprotocol.hpp
@@ -0,0 +1,85 @@
+/**************************************************************************
+** Copyright (C) 2010-2016 MS-Cheminformatics LLC
+*
+** Contact: toshi.hondo@qtplatz.com or info@ms-cheminfo.com
+**
+** Commercial Usage
+**
+** Licensees holding valid MS-Cheminformatics commercial licenses may use this file in
+** accordance with the MS-Cheminformatics Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and MS-Cheminformatics.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.TXT included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
+#pragma once
+
+#include "adcontrols/adcontrols_global.h"
+#include <boost/variant.hpp>
+#include <cstdint>
+#include <vector>
+#include <string>
+
+namespace boost { namespace serialization { class access; } }
+
+namespace adcontrols {
+
+    template<typename T> class TofProtocol_archive;
+        
+    //--
+    class ADCONTROLSSHARED_EXPORT TofProtocol {
+    public:
+        typedef boost::variant< int32_t, double > additional_value_type;
+        typedef std::pair< double, double > delay_pulse_type;
+        
+        enum MULTUM_PULSE_CONFIG { MULTUM_PUSH, MULTUM_INJECT, MULTUM_EXIT, MULTUM_GATE_0, MULTUM_GATE_1, EXT_ADC_TRIG };
+
+        std::vector< delay_pulse_type >& delay_pulses();
+        const std::vector< delay_pulse_type >& delay_pulses() const;
+
+        std::vector< std::pair< int32_t, additional_value_type > >& additionals();
+        const std::vector< std::pair< int32_t, additional_value_type > >& additionals() const;
+        
+        std::vector< std::string >& formulae();
+        const std::vector< std::string >& formulae() const;
+
+        void setDescription( const std::string& );
+        const std::string& description() const;
+
+        void setReference( uint32_t );
+        uint32_t reference() const;
+
+    private:
+        double lower_mass_;
+        double upper_mass_;
+        std::vector< delay_pulse_type > delay_pulses_;
+        std::vector< std::pair< int32_t, additional_value_type > > additionals_;
+        uint32_t reference_;                    // lock mass reference (bit position indicate which formula in formulae
+        std::vector< std::string > formulae_;   // formula list, separate with ';'
+        std::string description_;
+        
+    public:
+        TofProtocol();
+        TofProtocol( const TofProtocol& t );
+
+    private:
+        friend class boost::serialization::access;
+        template< class Archive >  
+            void serialize( Archive& ar, const unsigned int version );
+
+        friend class TofProtocol_archive < TofProtocol > ;
+        friend class TofProtocol_archive < const TofProtocol > ;
+    };
+
+};
+
+
