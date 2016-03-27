9febace319c33788b60125c1d46b0c5a3334d974
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2016-03-27 14:37:46 +0900

    boost component build on deb package

--- contrib/installer/bootstrap.sh
+++ contrib/installer/bootstrap.sh
@@ -7,7 +7,7 @@ target_arch=$cross_target
 if [ $# -eq 0 ]; then
     echo "usage:"
     echo "native"
-    echo "\t./bootstrap.sh [boost [qt5 [ace+tao]]]"
+    echo "\t./bootstrap.sh [boost[qt5 [ace+tao]]]]"
     echo "cross"
     echo "\tcross_target=[arm-linux-gnueabihf|raspi] ./bootstrap.sh [boost [qt5 [ace+tao]]]"
     exit
@@ -29,7 +29,7 @@ do
 	    mkdir -p build-$target_arch/$var
 	    if [ -z $cross_target ]; then
 		( cd build-$target_arch/$var;
-		  cmake -DCMAKE_PREFIX_PATH=/usr/local/boost-1_58 ../../$var )
+		  cmake -DCMAKE_PREFIX_PATH=/usr/local/boost-1_59 ../../$var )
 	    else
 		( cd build-$target_arch/$var;
 		  cmake -DCMAKE_TOOLCHAIN_FILE=../../../../toolchain-$target_arch.cmake ../../$var )
