248df1ea67734346f3526f7da0e3447a32c89824
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2016-05-31 09:23:43 +0900

    back x86 (32bit) build setup

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,24 +1,18 @@
 :#!cmd.exe
 @echo off
+
+setlocal enabledelayedexpansion
+
 set source_dir=%cd%
 set build_root=..\build-x86_64
 set build_type=release
 set build_tests=false
 set build_clean=false
+set build_x86=false
+set address_model=64
+set GENERATOR="Visual Studio 14 2015 Win64"
 
-echo %VisualStudioVersion%
-
-if %VisualStudioVersion% EQU 12.0 (
-   set tools=vc12
-   set QTDIR=C:\Qt\Qt5.6.0\5.6\msvc2015_64
-)
-
-if %VisualStudioVersion% EQU 14.0 (
-   set tools=vc14
-   set QTDIR=C:\Qt\5.5\msvc2013_64
-)
-
-echo "############ bootstrap building qtplatz using "%tools%" #############"
+echo "Visual Studio Version: " %VisualStudioVersion%
 
 for %%i in (%*) do (
     if %%i==release (
@@ -29,63 +23,63 @@ for %%i in (%*) do (
        set build_tests=true
     ) else if %%i==clean (
        set build_clean=true
+    ) else if %%i==x86 (
+      set build_root=..\build-x86
+      set address_model=32
     )
 )
 
-set build_dir=%build_root%\qtplatz.%build_type%_%tools%
+if %VisualStudioVersion% EQU 12.0 (
+   set tools=vc12
+   if %address_model% EQU 32 (
+      set GENERATOR="Visual Studio 12 2013"
+      set QTDIR=C:\Qt\5.6.0\5.6\msvc2013
+   ) else (
+      set GENERATORr="Visual Studio 12 2013 Win64"
+      set QTDIR=C:\Qt\5.6.0\5.6\msvc2013_64
+   )
+)
+
+if %VisualStudioVersion% EQU 14.0 (
+   set tools=vc14
+   if %address_model% EQU 32 (
+      set GENERATOR="Visual Studio 14 2015"
+      set QTDIR=C:\Qt\5.6\msvc2015
+   ) else (
+      set GENERATOR="Visual Studio 14 2015 Win64"
+      set QTDIR=C:\Qt\5.6\msvc2015_64
+   )
+)
+
+echo -------------- address_model: !address_model!
+echo -------------- tools:         !tools!
+echo -------------- GENERATOR:     !GENERATOR!
+
+echo "############ bootstrap building qtplatz using "!tools!" #############"
+
+set build_dir=!build_root!\qtplatz.!build_type!_!tools!
 
 if %build_clean%==true (
-  echo rmdir %build_dir% /s /q
-  rmdir %build_dir% /s /q
+  echo rmdir !build_dir! /s /q
+  rmdir !build_dir! /s /q
   goto end
 )
 
-mkdir %build_dir%
-cd %build_dir%
+mkdir !build_dir!
+cd !build_dir!
 
-if %tools%==vc12 (
-
-   if %build_type%==release (
-
-      cmake -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=ON %source_dir%
-
-   ) else if %build_type%==package (
-
-     cmake -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=OFF -DQTPLATZ_SUPPORT_CORBA:BOOL=ON %source_dir%
-     cd contrib\installer\wix
-     nmake help
-     goto end
-
-   ) else (
-     cmake -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Debug -DDEBUG_SYMBOL:BOOL=ON %source_dir%
-   )
-
-   if %build_tests%==true (
-      echo "Build tests directory on %cd%"
-      mkdir tests
-      cd tests
-      cmake -G "Visual Studio 12 Win64" %source_dir%\tests
-   )
-   
-) else if %tools%==vc14 (
-
-  if %build_type%==release (
-  
-     cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DQTPLATZ_SUPPORT_CORBA:BOOL=OFF -DDEBUG_SYMBOL:BOOL=ON %source_dir%
-
-  ) else if %build_type%==package (
-
-     cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DQTPLATZ_SUPPORT_CORBA:BOOL=OFF -DDEBUG_SYMBOL:BOOL=OFF %source_dir%
-
-     cd contrib\installer\wix
-     nmake help
-     goto end
-
-  ) else (
-     cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Debug %source_dir%
-  )
+if !build_type!==release (
+    echo cmake -G !GENERATOR! -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=ON %source_dir%
+    cmake -G !GENERATOR! -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=ON %source_dir%
+) else if !build_type!==package (
+    echo cmake -G !GENERATOR! -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=OFF %source_dir%
+    cmake -G !GENERATOR! -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=OFF %source_dir%
+    cd contrib\installer\wix
+    nmake help
+    goto end
 )
 
 cd %source_dir%
 
 :end
+endlocal
