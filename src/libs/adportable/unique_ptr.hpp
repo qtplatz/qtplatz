1159e86e9c6adf5f98ca8b8b5e060075bb0eefcf
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2016-06-20 21:05:28 +0900

    workaround for strange behavior of make_unique on clang build

--- src/libs/adportable/unique_ptr.hpp
+++ src/libs/adportable/unique_ptr.hpp
@@ -25,55 +25,12 @@
 
 #pragma once
 
-#if __APPLE__
-# define HAS_MAKE_UNIQUE 1
-#elif defined _MSC_VER && _MSC_VER >= 1900
-#  define HAS_MAKE_UNIQUE 1
-#elif __GNUC__ <= 4 && __GNUC_MINOR__ <= 8
-#  define HAS_MAKE_UNIQUE 0
-#elif  __cplusplus >= 201103L 
-#  define HAS_MAKE_UNIQUE 1
+#if ! __APPLE__
+
+# if ( defined _MSC_VER && _MSC_VER < 1900 ) || ( __GNUC__ <= 4 && __GNUC_MINOR__ <= 8 ) || ( __cplusplus < 201103L )
+
+#  include <compiler/make_unique.hpp>
+
+# endif
 #endif
 
-#if ! defined HAS_MAKE_UNIQUE
-# define HAS_MAKE_UNIQUE 0
-#endif
-
-#if ! HAS_MAKE_UNIQUE
-
-#include <cstddef>
-#include <memory>
-#include <type_traits>
-#include <utility>
-
-namespace std {
-    template<class T> struct _Unique_if {
-        typedef unique_ptr<T> _Single_object;
-    };
-
-    template<class T> struct _Unique_if<T[]> {
-        typedef unique_ptr<T[]> _Unknown_bound;
-    };
-
-    template<class T, size_t N> struct _Unique_if<T[N]> {
-        typedef void _Known_bound;
-    };
-
-    template<class T, class... Args>
-        typename _Unique_if<T>::_Single_object
-        make_unique(Args&&... args) {
-            return unique_ptr<T>(new T(std::forward<Args>(args)...));
-        }
-
-    template<class T>
-        typename _Unique_if<T>::_Unknown_bound
-        make_unique(size_t n) {
-            typedef typename remove_extent<T>::type U;
-            return unique_ptr<T>(new U[n]());
-        }
-
-    template<class T, class... Args>
-        typename _Unique_if<T>::_Known_bound
-        make_unique(Args&&...) = delete;
-}
-#endif
