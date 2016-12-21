b76a85cbb368342281e82ca30e5c55939efaa1a1
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2016-12-21 09:13:39 +0900

    add AqDrv4 and AgMD2 libraries as desiered

--- cmake/Modules/FindAgMD2.cmake
+++ cmake/Modules/FindAgMD2.cmake
@@ -1,26 +1,38 @@
-# find AqDrv4 -- Keysight AP240/DC110... driver
+# find AgMD2 -- Keysight U5303A driver
+
+set ( AgMD2_INCLUDE_DIR "AgMD2_INCLUDE_DIR-NOTFOUND" )
+set ( _lib "_lib-NOTFOUND" )
 
 if ( WIN32 )
-  #set( VXIPNPPATH "C:/Program Files/IVI Foundation/VISA" )
-  set( IVIROOTDIR "C:/Program Files/IVI Foundation/IVI" )
-  find_path( AgMD2_INCLUDE_DIR NAME AgMD2.h PATHS ${IVIROOTDIR}/include )
-  
-  if ( AqMD2_INCLUDE_DIR )
-    set( AgMD2_FOUND TRUE )
-    set( AqMD2_LIBRARIES AqMD2 )
-  endif()
+  set ( CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".dll" )
+  set ( IVIROOTDIR "C:/Program Files/IVI Foundation/IVI" )
 
+  find_path( AgMD2_INCLUDE_DIR NAMES AgMD2.h PATHS "${IVIROOTDIR}/include" )
+
+  find_library( _lib NAMES AgMD2 HINTS "${IVIROOTDIR}/Lib_x64/msc"  )
+  set ( _dll "_dll-NOTFOUND" )
+  find_library( _dll NAMES AgMD2_64 HINTS "${IVIROOTDIR}/bin" )
+
+  if ( _lib AND _dll )
+    set( AgMD2_FOUND TRUE )
+    add_library( AgMD2 SHARED IMPORTED )
+    set_target_properties( AgMD2 PROPERTIES IMPORTED_IMPLIB "${_lib}" IMPORTED_LOCATION "${_dll}" )
+    set( AgMD2_LIBRARIES AgMD2 )
+  endif()
+  
 elseif( APPLE )
 
+  set( AgMD2_FOUND FALSE )  
   # no driver supported
   
 else() # Linux
   
-  find_path( AgMD2_INCLUDE_DIR NAME AgMD2.h PATHS /usr/include )
-  find_library( AqMD2_LIBRARY AqMD2 )
-  if ( AqMD2_LIBRARY )
+  find_path( AgMD2_INCLUDE_DIR NAMES AgMD2.h PATHS /usr/include )
+  find_library( _lib NAMES AgMD2 )
+  if ( _lib )
     set( AgMD2_FOUND TRUE )
-  endif()
+    set( AgMD2_LIBRARIES ${_lib} )    
+  endif()    
 
 endif()
 
--- cmake/Modules/FindAqDrv4.cmake
+++ cmake/Modules/FindAqDrv4.cmake
@@ -1,26 +1,36 @@
 # find AqDrv4 -- Keysight AP240/DC110... driver
 
 if ( WIN32 )
-
   set( ACQRSDIR   "C:/Program Files (x86)/Agilent/Acqiris" )
-  find_path( AqDrv4_INCLUDE_DIR NAME AcqirisImport.h PATHS "${ACQRSDIR}/include" )
+  find_path( AqDrv4_INCLUDE_DIR NAMES AcqirisImport.h PATHS "${ACQRSDIR}/include" )
 
-  find_library( _lib NAME AqDrv4 PATHS "${ACQRSDIR}/bin" )
-  if ( _lib )
+  set( _lib "_lib-NOTFOUND" )
+  set( _dll "_dll-NOTFOUND" )
+  set ( CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".dll")
+  find_library( _lib NAMES AqDrv4_x64.lib PATHS "${ACQRSDIR}/lib" PATH_SUFFIXES ".lib" )
+  find_library( _dll NAMES AqDrv4_x64.dll PATHS "${ACQRSDIR}/bin" )
+
+  if ( _lib AND _dll )
     set( AqDrv4_FOUND TRUE )
-    set( AqDrv4_LIBRARIES ${_lib} )
+    add_library( AqDrv4 SHARED IMPORTED )
+    set_target_properties( AqDrv4 PROPERTIES IMPORTED_IMPLIB "${_lib}" IMPORTED_LOCATION "${_dll}")
+    set( AqDrv4_LIBRARIES AqDrv4 )
   endif()
 
 elseif( APPLE )
 
-  find_path( AqDrv4_INCLUDE_DIR NAME AcqirisImport.h PATHS ${CMAKE_SOURCE_DIR}/contrib/agilent/include )
+  find_path( AqDrv4_INCLUDE_DIR NAMES AcqirisImport.h PATHS ${CMAKE_SOURCE_DIR}/contrib/agilent/include )
+  set( AqDrv4_FOUND FALSE )
   
 else() # Linux
   
-  find_path( AqDrv4_INCLUDE_DIR NAME AcqirisImport.h PATHS /usr/include ${CMAKE_SOURCE_DIR}/contrib/agilent/include )
-  find_library( AqDrv4_LIBRARY AqDrv4 )
-  if ( AqDrv4_LIBRARY )
+  find_path( AqDrv4_INCLUDE_DIR NAMES AcqirisImport.h PATHS ${CMAKE_SOURCE_DIR}/contrib/agilent/include )
+
+  set( _lib "_lib-NOTFOUND" )  
+  find_library( _lib AqDrv4 )
+  if ( _lib )
     set( AqDrv4_FOUND TRUE )
+    set( AqDrv4_LIBRARIES ${_lib} )    
   endif()
 
 endif()
