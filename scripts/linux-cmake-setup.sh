825301594344918049e1e7691fc6a9dc6a9dfb24
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2017-04-24 20:56:36 +0900

    script bug fixed

--- scripts/linux-cmake-setup.sh
+++ scripts/linux-cmake-setup.sh
@@ -9,7 +9,6 @@ if type cmake > /dev/null; then
     prompt
 fi
 
-exit
 echo "=========="
 echo "building cmake"
 
@@ -18,12 +17,14 @@ if [ ! -d ~/src/cmake ]; then
 	mkdir ~/src
     fi
     # git clone https://gitlab.kitware.com/cmake/cmake ~/src/cmake
-    ( cd ~/Downloads;
-      wget https://cmake.org/files/v3.8/cmake-3.8.0.tar.gz )
-    tar xvf ~/Downlodads/cmake-3.8.0.tar.gz -C ~/src
+    if [ ! -f ~/Downloads/cmake-3.8.0.tar.gz ]; then
+      ( cd ~/Downloads;
+        wget https://cmake.org/files/v3.8/cmake-3.8.0.tar.gz )
+    fi
+    tar xvf ~/Downloads/cmake-3.8.0.tar.gz -C ~/src
 fi
 
-cd ~/src/cmake
+cd ~/src/cmake-3.8.0
 ./bootstrap
 if make -j4; then
     sudo make install
