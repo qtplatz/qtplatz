1cd9f6d9791b9e1a90800dee2b1dba12b4b5d408
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2017-09-13 18:48:26 +0900

    trial for merging ap240/u5303 threshold results

---
+++ contrib/agilent/libs/acqrscontrols/threshold_result.cpp
@@ -0,0 +1,111 @@
+/**************************************************************************
+** Copyright (C) 2010-2017 Toshinobu Hondo, Ph.D.
+** Copyright (C) 2013-2017 MS-Cheminformatics LLC, Toin, Mie Japan
+*
+** Contact: toshi.hondo@qtplatz.com
+**
+** Commercial Usage
+**
+** Licensees holding valid ScienceLiaison commercial licenses may use this file in
+** accordance with the MS-Cheminformatics Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and MS-Cheminformatics LLC.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.TXT included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
+#include "threshold_result.hpp"
+
+#include <adportable/counting/threshold_index.hpp>
+#include <adportable/counting/counting_result.hpp>
+#include <adportable/portable_binary_iarchive.hpp>
+#include <adportable/portable_binary_oarchive.hpp>
+#include <boost/format.hpp>
+#include <boost/iostreams/device/array.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/version.hpp>
+#include <memory>
+#include <vector>
+#include <cstdint>
+#include <ostream>
+
+using namespace acqrscontrols;
+
+threshold_result::threshold_result() : foundIndex_( npos )
+                                     , findRange_( 0, 0 )
+                                     , findUp_( false )
+{
+}
+
+std::vector< double >&
+threshold_result::processed()
+{
+    processed_;
+}
+    
+const std::vector< double >&
+threshold_result::processed() const
+{
+    processed_;            
+}
+
+const std::pair<uint32_t, uint32_t >&
+threshold_result::findRange() const
+{
+    return findRange_;
+}
+    
+uint32_t
+threshold_result::foundIndex() const
+{
+    return foundIndex_;
+}
+    
+void
+threshold_result::setFoundAction( uint32_t index, const std::pair< uint32_t, uint32_t >& range )
+{
+    foundIndex_ = index;
+    findRange_ = range;
+}
+    
+void
+threshold_result::setFindUp( bool value )
+{
+    findUp_ = value;
+}
+    
+bool
+threshold_result::findUp() const
+{
+    return findUp_;
+}
+
+bool
+threshold_result::deserialize( const int8_t * xdata, size_t dsize )
+{
+    // restore indecies
+    boost::iostreams::basic_array_source< char > device( reinterpret_cast< const char *>(xdata), dsize );
+    boost::iostreams::stream< boost::iostreams::basic_array_source< char > > st( device );
+    
+    try {
+        portable_binary_iarchive ar( st );
+        ar >> indecies_;
+    } catch ( std::exception& ) {
+        return false;
+    }
+    
+    return true;
+}
---
+++ contrib/agilent/libs/acqrscontrols/threshold_result.hpp
@@ -0,0 +1,109 @@
+/**************************************************************************
+** Copyright (C) 2010-2017 Toshinobu Hondo, Ph.D.
+** Copyright (C) 2013-2017 MS-Cheminformatics LLC, Toin, Mie Japan
+*
+** Contact: toshi.hondo@qtplatz.com
+**
+** Commercial Usage
+**
+** Licensees holding valid ScienceLiaison commercial licenses may use this file in
+** accordance with the MS-Cheminformatics Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and MS-Cheminformatics LLC.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.TXT included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
+#pragma once
+
+#include "acqrscontrols_global.hpp"
+#include <adportable/counting/threshold_index.hpp>
+#include <adportable/counting/counting_result.hpp>
+#include <boost/serialization/version.hpp>
+#include <boost/serialization/access.hpp>
+#include <memory>
+#include <vector>
+#include <cstdint>
+#include <ostream>
+#include <compiler/pragma_warning.hpp>
+
+// this class was derived from both ap240::threshold_result and u5303a::threshold_result
+// for AP240 support on infitof2
+// 2017-SEP-18
+
+namespace acqrscontrols {
+
+    class threshold_result {
+    public:
+        
+# if defined _MSC_VER && _MSC_VER <= 1800
+        static const uint32_t npos = (-1);
+# else
+        static constexpr uint32_t npos = ( -1 );
+# endif
+        
+        threshold_result();
+        threshold_result( const threshold_result& t );
+
+        std::vector< double >& processed();
+        const std::vector< double >& processed() const;
+        const std::pair<uint32_t, uint32_t >& findRange() const;
+        uint32_t foundIndex() const;
+        void setFoundAction( uint32_t index, const std::pair< uint32_t, uint32_t >& );
+        void setFindUp( bool value );
+        bool findUp() const;
+        
+        bool deserialize( const int8_t * data, size_t dsize );
+        
+        static void write3( std::ostream&, const threshold_result& );
+        
+    private:
+        std::vector< uint32_t > indecies_;
+        std::vector< double > processed_;
+        std::pair< uint32_t, uint32_t > findRange_;
+        uint32_t foundIndex_;
+        bool findUp_;
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    template< typename waveform_type >
+    class ACQRSCONTROLSSHARED_EXPORT threshold_result_ : public threshold_result
+                                                       , public adportable::counting::counting_result {
+    public:
+        threshold_result_() {}
+        
+        threshold_result_( std::shared_ptr< const waveform_type > d )
+            : data_( d ) {
+        }
+        
+        threshold_result_( const threshold_result_& t ) : threshold_result( t )
+            , adportable::counting::counting_result( t )
+            , data_( t.data_ ) {
+        }
+        
+        std::shared_ptr< const waveform_type >& data()      { return data_;  }
+        std::shared_ptr< const waveform_type > data() const { return data_; }
+
+        bool deserialize( const int8_t * xdata, size_t dsize, const int8_t * xmeta, size_t msize ) {
+            //
+            // see threshold_result_accessor.cpp in infitof/plugins/infitof2
+            //
+            auto data = std::make_shared< waveform_type >();
+            data->deserialize_xmeta( reinterpret_cast<const char *>( xmeta ), msize );
+            data_ = std::move( data );
+            return threshold_result::deserialize( xdata, dsize );
+        }
+
+    private:
+        std::shared_ptr< const waveform_type > data_;
+    };
+}
