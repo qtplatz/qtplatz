afa61648c26d2ba65c8326f9f4ed42d66e7ea1bb
Toshinobu Hondo <toshi@malpix.mass.phys.sci.osaka-u.ac.jp>
2017-09-19 06:45:45 +0900

    scripts minor changes

--- contrib/agilent/acqiris/install-firmware.sh
+++ contrib/agilent/acqiris/install-firmware.sh
@@ -5,7 +5,7 @@ if [ -z $AcqirisCD ]; then
     exit 1
 fi
 
-sudo mkdir /usr/lib/firmware/aqrs
+sudo mkdir -p /usr/lib/firmware/aqrs
 sudo cp -p $AcqirisCD/Firmware/*.bit /usr/lib/firmware/aqrs
 sudo cp AqDrv4.ini /etc
 sudo sh -c "echo 'export AcqirisDxDir=/etc' >> /etc/profile.d/ap240.sh"
