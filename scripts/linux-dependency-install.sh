ca65922c4dcaa9b3aeffa33a1146447ce6fcf602
Toshinobu Hondo <toshi@malpix.mass.phys.sci.osaka-u.ac.jp>
2017-09-18 18:04:02 +0900

    move qt5 dependency install to linux-dependency-install.sh

--- scripts/linux-dependency-install.sh
+++ scripts/linux-dependency-install.sh
@@ -2,23 +2,56 @@
 
 failed_list=()
 
+#qt5
+list_dependency+=('mesa-common-dev'
+		  'libglu1-mesa-dev'
+		  'freeglut3-dev'
+		  'libxml2-dev'
+		  'libxslt1-dev'
+		  'libgstreamer-plugins-base0.10'
+		  'python2.7'
+		  'python2.7-dev')
+
 #rtags 
-list_dependency=('clang-3.9' 'lldb-3.9' 'libclang-3.9-dev')
+list_dependency+=('clang-3.9'
+		  'lldb-3.9'
+		  'libclang-3.9-dev')
 
 #cmake+opencv
-list_dependency=('libcurl4-openssl-dev' 'libhdf5-dev')
+list_dependency+=('libcurl4-openssl-dev'
+		  'libhdf5-dev')
 
 #jpeg
-list_dependency=('nasm' 'libass-dev' 'libfdk-aac-dev')
+list_dependency+=('nasm'
+		  'libass-dev'
+		  'libfdk-aac-dev')
 
 #ffmpeg
-list_dependency+=('lame' 'libtheora-dev' 'libvorbis-dev'
-			'libvpx-dev' 'libopus-dev' 'yasm' 'x264' 'libx264-dev' 'x265' 'libx265-dev'
-			'libmp3lame0' 'libmp3lame-dev' )
+list_dependency+=('lame'
+		  'libtheora-dev'
+		  'libvorbis-dev'
+		  'libvpx-dev'
+		  'libopus-dev'
+		  'yasm' 'x264'
+		  'libx264-dev'
+		  'x265'
+		  'libx265-dev'
+		  'libmp3lame0'
+		  'libmp3lame-dev' )
 #opencv
-list_dependency+=('python-dev' 'python-numpy' 'libtbb2' 'libtbb-dev' 'libpng-dev' 'libtiff-dev'
-		  'libpng12-dev' 'libtiff5-dev' 'libgtk2.0-dev' 'pkg-config' 'libavcodec-dev'
-		  'libavformat-dev' 'libswscale-dev' )
+list_dependency+=('python-dev'
+		  'python-numpy'
+		  'libtbb2'
+		  'libtbb-dev'
+		  'libpng-dev'
+		  'libtiff-dev'
+		  'libpng12-dev'
+		  'libtiff5-dev'
+		  'libgtk2.0-dev'
+		  'pkg-config'
+		  'libavcodec-dev'
+		  'libavformat-dev'
+		  'libswscale-dev' )
 
 #echo "Install dependency"
 list_dependency+=('libfreeimage-dev'
@@ -43,27 +76,3 @@ if [ ${#failed_list[@]} -gt 0 ]; then
    done
 fi
 
-#echo "Install dependency for for ffmpeg"
-#sudo apt install -y nasm
-#sudo apt install libass-dev
-#sudo apt install libfdk-aac-dev
-##sudo apt install FreeType-2.8, LAME-3.99.5,
-#sudo apt install lame
-#sudo apt install libtheora-dev
-#sudo apt install libvorbis-dev
-#sudo apt install libvpx-dev
-#sudo apt install libopus-dev
-#sudo apt install yasm
-#sudo apt install x264
-#sudo apt install libx264-dev
-#sudo apt install x265
-#sudo apt install libx265-dev
-#sudo apt install libmp3lame0
-#sudo apt install libmp3lame-dev
-
-##dependency for opencv
-#echo "Install dependency for OpenCV"
-#sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev # libjpeg-dev
-#sudo apt-get install -y libpng-dev libtiff-dev
-#sudo apt-get install -y libpng12-dev libtiff5-dev
-#sudo apt-get install -y libgtk2.0 pkg-config libavcodec-dev libavformat-dev libswscale-dev
--- scripts/linux-initial-setup.sh
+++ scripts/linux-initial-setup.sh
@@ -9,19 +9,9 @@ if [ $staff -eq 0 ]; then
     echo "You may require logout and login again for usermod take effect"
 fi
 
+sudo apt-get -y insgall git
 sudo apt-get -y install build-essential
 sudo apt-get -y install dkms
-
+sudo apt-get -y install bc
 sudo apt-get -y install libncurses5-dev
-sudo apt-get -y insgall git
-sudo apt-get -y install mesa-common-dev
-sudo apt-get -y install libglu1-mesa-dev freeglut3-dev
-sudo apt-get -y install libxml2-dev libxslt1-dev
-sudo apt-get -y install libgstreamer-plugins-base0.10
-sudo apt-get -y install python2.7 python2.7-dev
 
-echo "==========================="
-echo "  You can now install Qt5  "
-echo "  Don't forget to do 'ln -s /opt/Qt/5.8/gcc_64/bin/qmake /usr/local/bin'"
-echo "  And delete /usr/bin/qmake"
-echo "==========================="
