a7851451abb9f231cb79462d85f59d75bfb82e20
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2017-12-06 11:10:35 +0900

    improve bootstrap.bat

--- bootstrap.bat
+++ bootstrap.bat
@@ -6,37 +6,56 @@ setlocal enabledelayedexpansion
 if %VisualStudioVersion% EQU 14.0 (
    set tools=vc14
    set GENERATOR="Visual Studio 14 2015 Win64"
-   set build_dir=%src_dir%\build-vc14-x86_64
    set QTDIR=C:\Qt\5.9.3\msvc2015_64
 )
+
 if %VisualStudioVersion% EQU 15.0 (
    set tools=vc15
    set GENERATOR="Visual Studio 15 2017 Win64"
-   set build_dir=%src_dir%\build-vc15-x86_64
    set QTDIR=C:\Qt\5.9.3\msvc2017_64
 )
 
-set source_dir=%cd%
-set build_root=..
+set source_dir="%~dp0"
+set build_root="%~dp0.."
 set build_arch=x86_64
-set build_type=release
+set build_target=release
 set build_tests=false
 set build_clean=false
-
-echo "Visual Studio Version: " %VisualStudioVersion%
+set query_build_dir=false
+set exec_build=false
 
 for %%i in (%*) do (
     if %%i==release (
-       set build_type=release
+       set build_target=release
     ) else if %%i==package (
-       set build_type=package
+       set build_target=package
     ) else if %%i==tests (
        set build_tests=true
     ) else if %%i==clean (
        set build_clean=true
     ) else if %%i==x86 (
       set build_arch=x86
-    )
+    ) else if "%%i"=="--query-source_dir" (
+      @echo %source_dir%
+      goto end
+    ) else if "%%i"=="--query-build_root" (
+      @echo %build_root%
+      goto end
+    ) else if "%%i"=="--query-build_dir" (
+      set query_build_dir=true
+    ) else if "%%i"=="--build" (
+      set exec_build=true
+    ) else if "%%i"=="--package" (
+      set exec_build=true
+      set build_target=package
+    )      
+)
+
+set build_dir=!build_root!\build-!tools!-!build_arch!\qtplatz.!build_target!
+
+if %query_build_dir%==true (
+   @echo %build_dir%
+   goto end
 )
 
 echo -------------- arch:          !build_arch!
@@ -45,31 +64,31 @@ echo -------------- GENERATOR:     !GENERATOR!
 
 echo "############ bootstrap building qtplatz using "!tools!" #############"
 
-set build_dir=!build_root!\build-!tools!-!build_arch!\qtplatz.!build_type!
-
 if %build_clean%==true (
   echo rmdir !build_dir! /s /q
   rmdir !build_dir! /s /q
   goto end
 )
 
-mkdir !build_dir!
+if not exist !build_dir! (
+   mkdir !build_dir!
+)
+
 cd !build_dir!
 
-if !build_type!==release (
+if !build_target!==release (
     echo cmake -G !GENERATOR! -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=ON %source_dir%
     cmake -G !GENERATOR! -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=ON %source_dir%
     cd %source_dir%
-) else if !build_type!==package (
+) else if !build_target!==package (
     echo cmake -G !GENERATOR! -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=OFF %source_dir%
     cmake -G !GENERATOR! -DCMAKE_BUILD_TYPE=Release -DDEBUG_SYMBOL:BOOL=OFF %source_dir%
-::    cd contrib\installer\wix
-::    nmake help
 )
 
-echo %cd%
+if "%exec_build%"=="true" (
+   nmake -C !build_dir! !build_target!
+)
+
 :end
 
-endlocal & set build_dir=%build_dir% & set source_dir=%source_dir%
-
-
+endlocal
