d633dd28794ed848a95ffd4735958c30360550b1
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2018-01-27 08:23:30 +0900

    misc

--- scripts/i386/debian-i386-modelsim-dependency.sh
+++ scripts/i386/debian-i386-modelsim-dependency.sh
@@ -18,3 +18,11 @@ echo "==========================="
 echo dpkg --print-foreign-architectures
 dpkg --print-foreign-architectures
 echo "==========================="
+
+echo "wget http://download.savannah.gnu.org/releases/freetype/freetype-2.4.12.tar.bz2"
+echo "tar freetype-2.4.12.tar.bz2 -C ~/src/"
+echo "cd ~/src/freetype-2.4.12"
+echo "./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32""
+echo "make -j8"
+
+echo "open http://mattaw.blogspot.jp/2014/05/making-modelsim-altera-starter-edition.html"
