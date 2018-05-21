3f912dd3665bb9eb8378724065da3bbe068085ce
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2018-05-22 07:17:27 +0900

    fix bzip2 boost issue

--- scripts/windows-boost-setup.bat
+++ scripts/windows-boost-setup.bat
@@ -16,20 +16,20 @@ for /F "tokens=1,2,3 delims=_" %%a in ("%BOOST_VERSION%") do (
 set BOOST_ROOT=C:\Boost\include\boost-%BOOST_VERSION_Major%_%BOOST_VERSION_Minor%_%BOOST_VERSION_Revision%
 set BOOST_LIBRARY_DIR=C:\Boost\lib
 set BOOST_BUILD_DIR=%SOURCE_ROOT%\boost_%BOOST_VERSION%
-set BZIP2_SOURCE_DIR=%SOURCE_ROOT%\bzip2-1.0.6
+set BZIP2DIR=%SOURCE_ROOT%\bzip2-1.0.6
 
 echo "Installing BOOST: BOOST_VERSION=%BOOST_VERSION%"
 echo "Install to      : BOOST_ROOT=%BOOST_ROOT%"
 echo "Buld in         : BOOST_BUILD_DIR=%BOOST_BUILD_DIR%"
-echo "Additional      : BZIP2_SOURCE_DIR=%BZIP2_SOURCE_DIR%"
+echo "Additional      : BZIP2DIR=%BZIP2DIR%"
 
 if %VisualStudioVersion% EQU 14.0 ( set msvc=msvc-14.0 )
 if %VisualStudioVersion% EQU 15.0 ( set msvc=msvc-14.1 )
 
 :Expecting git-bash installed, and enable tools from windows command prompt
 
-if NOT EXIST %BZIP2_SOURCE_DIR% (
-   echo "%BZIP2_SOURCE_DIR% does not exist."
+if NOT EXIST %BZIP2DIR% (
+   echo "%BZIP2DIR% does not exist."
    set tarball=bzip2-1.0.6.tar.gz
    if NOT EXIST %SOURCE_ROOT%/%tarball% (
       echo "downloading http://www.bzip.org/1.0.6/%tarball%"
@@ -66,14 +66,16 @@ if %BOOST_VERSION_Minor% gtr "65" (
    goto simple
 )
 
-b2 -j%nproc% toolset=%msvc% architecture=x86 address-model=64 -s BZIP2_SOURCE=%bzip2_dir% threading=multi runtime-link=shared --build-type=minimal link=static --stagedir=stage/x86_64 stage
-b2 -j%nproc% toolset=%msvc% architecture=x86 address-model=64 -s BZIP2_SOURCE=%bzip2_dir% threading=multi runtime-link=shared --build-type=minimal link=shared --stagedir=stage/x86_64 stage install
+b2 -j%NUMBER_OF_PROCESSORS% toolset=%msvc% architecture=x86 address-model=64 -s BZIP2_SOURCE=%BZIP2DIR% ^
+   threading=multi runtime-link=shared --build-type=minimal link=static --stagedir=stage/x86_64 stage
+b2 -j%NUMBER_OF_PROCESSORS% toolset=%msvc% architecture=x86 address-model=64 -s BZIP2_SOURCE=%BZIP2DIR% ^
+   threading=multi runtime-link=shared --build-type=minimal link=shared --stagedir=stage/x86_64 stage install
 
 goto end
 
 :simple
-b2 -j%NUMBER_OF_PROCESSORS% address-model=64 -s BZIP2_SOURCE=%bzip2_dir% link=static --stagedir=stage/x64-static stage install
-b2 -j%NUMBER_OF_PROCESSORS% address-model=64 -s BZIP2_SOURCE=%bzip2_dir% link=shared --stagedir=stage/x64-shared stage install
+b2 -j%NUMBER_OF_PROCESSORS% address-model=64 -s BZIP2_SOURCE=%BZIP2DIR% link=static --stagedir=stage/x64-static stage install
+b2 -j%NUMBER_OF_PROCESSORS% address-model=64 -s BZIP2_SOURCE=%BZIP2DIR% link=shared --stagedir=stage/x64-shared stage install
 
 :end
 
