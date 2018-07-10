5203b9708e184e656a569b6c6ddcab066ba77ea0
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2018-07-10 20:14:43 +0900

    add date.h into deb package

--- cmake/Modules/boost-cpack.cmake
+++ cmake/Modules/boost-cpack.cmake
@@ -7,23 +7,27 @@ include( "soname" )
 
 if ( Boost_FOUND )
 
-  set ( libs
-    atomic
-    bzip2
-    chrono
-    date_time
-    filesystem
-    iostreams
-    locale
-    log
-    log_setup
-    program_options
-    regex
-    serialization
-    system
-    thread
-    timer
-    wserialization )
+  if ( CMAKE_CROSSCOMPILING )
+    set ( libs bzip2 )
+  else()
+    set ( libs
+      atomic
+      bzip2
+      chrono
+      date_time
+      filesystem
+      iostreams
+      locale
+      log
+      log_setup
+      program_options
+      regex
+      serialization
+      system
+      thread
+      timer
+      wserialization )
+  endif()
 
   foreach ( lib ${libs} )
     file( GLOB files ${Boost_LIBRARY_DIRS}/libboost_${lib}.${SO}* )
