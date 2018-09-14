d779db5afc0402f9c9ee932118ea57943ab2ce4d
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2018-09-14 09:01:43 +0900

    refactor python code for readability

--- contrib/datautils/epochtime.py
+++ contrib/datautils/epochtime.py
@@ -14,11 +14,12 @@ ftr = [3600,60,1]
 
 for row in result_set:
     print "%s, %s, %f" % ( row[0], row[1], row[2]*1e-9 )
-    timeOfInj = row[2] / 1.0e9 # epoch_time := value taken from AcquiredData (seconds)
-    
-    midNight = time.mktime( datetime.strptime( row[1].split()[0], "%Y-%b-%d" ).timetuple() )
-    timeStr = row[1].split()[1]
-    ctime = sum( [ a*b for a,b in zip(ftr, map( float, timeStr.split(':') )) ] )
-    ftime = ctime #+ midNight
-
-    print "%.6f %.6f diff= %.2fh" % ( ftime, timeOfInj, ( ftime - timeOfInj ) / 3600 )
+    time_of_inject_since_epoch = row[2] / 1.0e9 # epoch_time := value taken from AcquiredData (seconds)
+    posix_time_of_midnight = time.mktime( datetime.strptime( row[1].split()[0], "%Y-%b-%d" ).timetuple() )
+    day_time_of_startrun = sum( [ a*b for a,b in zip(ftr, map( float, row[1].split()[1].split(':') )) ] )
+    posix_time_of_startrun = posix_time_of_midnight + day_time_of_startrun
+    tp_startrun = datetime.fromtimestamp( posix_time_of_startrun )
+    print "file created at: %.3f" % ( day_time_of_startrun )
+    print "startrun       : %.3f  %s" % ( posix_time_of_startrun, tp_startrun )
+    print "time difference: %.3f  %s" % ( posix_time_of_startrun - time_of_inject_since_epoch
+                                          , datetime.fromtimestamp( posix_time_of_startrun - time_of_inject_since_epoch )  )
