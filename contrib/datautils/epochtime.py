0e761cae48b9aee7ab808a367f4e7706c1b76082
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2018-09-14 09:25:04 +0900

    refactor python code for gnuplot frendly output

--- contrib/datautils/epochtime.py
+++ contrib/datautils/epochtime.py
@@ -11,15 +11,25 @@ c.execute( "select * from results" )
 result_set = c.fetchall()
 
 ftr = [3600,60,1]
+prev_time_of_inject_since_epoch = 0
+prev_posix_time_of_startrun = 0
+print "\"file\"\t\"created\"\t\"startrun\"\t\"inject\", \"injection-interval\", \"ctime interval\""
 
 for row in result_set:
-    print "%s, %s, %f" % ( row[0], row[1], row[2]*1e-9 )
+    # print "%s, %s, %f" % ( row[0], row[1], row[2]*1e-9 )
     time_of_inject_since_epoch = row[2] / 1.0e9 # epoch_time := value taken from AcquiredData (seconds)
     posix_time_of_midnight = time.mktime( datetime.strptime( row[1].split()[0], "%Y-%b-%d" ).timetuple() )
     day_time_of_startrun = sum( [ a*b for a,b in zip(ftr, map( float, row[1].split()[1].split(':') )) ] )
     posix_time_of_startrun = posix_time_of_midnight + day_time_of_startrun
     tp_startrun = datetime.fromtimestamp( posix_time_of_startrun )
-    print "file created at: %.3f" % ( day_time_of_startrun )
-    print "startrun       : %.3f  %s" % ( posix_time_of_startrun, tp_startrun )
-    print "time difference: %.3f  %s" % ( posix_time_of_startrun - time_of_inject_since_epoch
-                                          , datetime.fromtimestamp( posix_time_of_startrun - time_of_inject_since_epoch )  )
+    duration_inject = time_of_inject_since_epoch - prev_time_of_inject_since_epoch
+    duration_ctime  = posix_time_of_startrun - prev_posix_time_of_startrun
+    print "\"%s\"\t\"%s\"\t%.3f\t%.3f\t%3f\t%3f" %  ( row[0]
+                                                      , tp_startrun
+                                                      , posix_time_of_startrun
+                                                      , time_of_inject_since_epoch
+                                                      , duration_inject
+                                                      , duration_ctime )
+    prev_time_of_inject_since_epoch = time_of_inject_since_epoch
+    prev_posix_time_of_startrun = posix_time_of_startrun
+
