f098947a72291548d9a850d2084e1f6ae41fa7af
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2019-01-17 18:17:55 +0900

    update

--- src/tools/adimport/main.cpp
+++ src/tools/adimport/main.cpp
@@ -1,7 +1,6 @@
-
 /**************************************************************************
-** Copyright (C) 2010-2018 Toshinobu Hondo, Ph.D.
-** Copyright (C) 2013-2018 MS-Cheminformatics LLC, Toin, Mie Japan
+** Copyright (C) 2010-2019 Toshinobu Hondo, Ph.D.
+** Copyright (C) 2013-2019 MS-Cheminformatics LLC, Toin, Mie Japan
 *
 ** Contact: toshi.hondo@qtplatz.com
 **
@@ -24,6 +23,9 @@
 **************************************************************************/
 
 #include "dataprocessor.hpp"
+#include <adplugins/adtextfile/dialog.hpp>
+#include <adplugins/adtextfile/txtspectrum.hpp>
+#include <adplugins/adtextfile/time_data_reader.hpp>
 #include <acqrscontrols/u5303a/waveform.hpp>
 #include <adcontrols/datareader.hpp>
 #include <adcontrols/lcmsdataset.hpp>
@@ -39,22 +41,34 @@
 #include <adportable/binary_serializer.hpp>
 #include <adportable/debug.hpp>
 #include <adportable/spectrum_processor.hpp>
+#include <adportable/textfile.hpp>
+#include <adportable/utf.hpp>
+#include <adportfolio/portfolio.hpp>
+#include <adportfolio/folder.hpp>
+#include <adportfolio/folium.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/format.hpp>
 #include <boost/program_options.hpp>
+#include <boost/tokenizer.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <ratio>
+#include <QApplication>
+#include <QMessageBox>
+#include <QString>
+#include <QStringList>
 
 namespace po = boost::program_options;
 
 int
 main(int argc, char *argv[])
 {
+    QApplication a(argc, argv);
+
     po::variables_map vm;
-    po::options_description description( "counting" );
+    po::options_description description( "adimport" );
     {
         description.add_options()
             ( "help,h",      "Display this help message" )
@@ -75,7 +89,7 @@ main(int argc, char *argv[])
     adplugin::manager::standalone_initialize();
 
     ::adfs::filesystem fs;
-    
+
     boost::filesystem::path outfile( "output.adfs" );
     if ( vm.count( "output" ) )
         outfile = boost::filesystem::path( vm[ "output" ].as< std::string >() );
@@ -87,21 +101,89 @@ main(int argc, char *argv[])
         if ( ! fs.create( outfile ) )
             return 1;
     }
-    
+
 	auto folder = fs.addFolder( L"/Processed/Spectra" );
+    auto filelist = vm[ "args" ].as< std::vector< std::string > >();
+    if ( filelist.empty() )
+        return 0;
+
+    //--- determine file type
+    adtextfile::Dialog dlg;
+    do {
+        QStringList models;
+        for ( auto& model: adcontrols::MassSpectrometer::get_model_names() )
+            models << QString::fromStdWString( model );
+        dlg.setDataInterpreterClsids( models );
+
+        boost::filesystem::path path( filelist[0] );
+        dlg.setDataType( adtextfile::Dialog::data_spectrum );
+
+        boost::filesystem::ifstream in( path );
+        if ( in.fail() ) {
+            ADDEBUG() << "Cannot open fil: " << path.string();
+            return 1;
+        }
+        typedef boost::char_separator<char> separator;
+        typedef boost::tokenizer< separator > tokenizer;
+        separator sep( ", \t", "", boost::drop_empty_tokens );
+        std::string line;
+        size_t nlines = 50;
+        while ( nlines-- && adportable::textfile::getline( in, line ) ) {
+            tokenizer tokens( line, sep );
+            QStringList list;
+            for ( tokenizer::iterator it = tokens.begin(); it != tokens.end(); ++it )
+                list << QString::fromStdString( *it );
+            dlg.appendLine( list );
+        }
+    } while ( 0 );
+
+    QApplication::changeOverrideCursor( Qt::ArrowCursor );
+    dlg.show();
+    double accelVoltage(0);
+    double length(0);
+    double tDelay(0);
+    std::string model;
+
+    if ( dlg.exec() ) {
+
+        QApplication::changeOverrideCursor( Qt::WaitCursor );
+
+        if ( dlg.hasScanLaw() ) {
+            accelVoltage = dlg.acceleratorVoltage();
+            length = dlg.length();
+            tDelay = dlg.tDelay();
+            model = dlg.dataInterpreterClsid().toStdString();
+        }
+
+        if ( dlg.dataType() != adtextfile::Dialog::data_spectrum ) {
+            return 1;
+            // TXTSpectrum txt;
+            // if ( txt.load( filename, dlg ) && prepare_portfolio( txt, filename, portfolio ) ) {
+            //     processedDataset_.reset( new adcontrols::ProcessedDataset );
+            //     processedDataset_->xml( portfolio.xml() );
+            //     return true;
+            // }
+        }
+    }
+
+    // -- end file type determination
 
     if ( vm.count("args") ) {
-        
+
         for ( auto& fname: vm[ "args" ].as< std::vector< std::string > >() ) {
-            
+
             boost::filesystem::path path( fname );
 
             ADDEBUG() << fname;
-            
+            if ( auto file = adcontrols::datafile::open( path.wstring(), false ) ) {
+
+            }
+
+
             // if ( path.extension() == ".adfs" ) {
 
             //     std::cout << path.string() << std::endl;
-                
+
             //     if ( auto file = adcontrols::datafile::open( path.wstring(), false ) ) {
             //         tools::dataprocessor processor;
             //         file->accept( processor );
@@ -117,4 +199,3 @@ main(int argc, char *argv[])
         }
     }
 }
-
