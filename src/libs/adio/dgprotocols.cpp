b9475363f1dae8eec0973f4d16dea9d26cffb227
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2019-04-28 09:41:04 +0900

    add agmodinst dma.c patch

--- src/libs/adio/dgprotocols.cpp
+++ src/libs/adio/dgprotocols.cpp
@@ -40,18 +40,17 @@
 //     }
 // }
 
-static void
-print( const boost::property_tree::ptree& pt )
-{
-    using boost::property_tree::ptree;
+// static void
+// print( const boost::property_tree::ptree& pt )
+// {
+//     using boost::property_tree::ptree;
 
-    ptree::const_iterator end = pt.end();
-    for (ptree::const_iterator it = pt.begin(); it != end; ++it) {
-        std::cout << it->first << ": " << it->second.get_value<std::string>() << std::endl;
-        print(it->second);
-    }    
-}
-
+//     ptree::const_iterator end = pt.end();
+//     for (ptree::const_iterator it = pt.begin(); it != end; ++it) {
+//         std::cout << it->first << ": " << it->second.get_value<std::string>() << std::endl;
+//         print(it->second);
+//     }
+// }
 
 // using namespace adportable::dg;
 
@@ -69,12 +68,12 @@ namespace adio {
                 boost::property_tree::read_json( json, pt );
 
                 protocols.interval_ = std::stod( pt.get_child( "protocols.interval" ).data() ) * 1.0e-6; // us -> seconds
-                
+
                 for ( const auto& v : pt.get_child( "protocols.protocol" ) ) {
 
                     protocol<delay_pulse_count> data;
 
-                    int index = std::stoi( v.second.get_child( "index" ).data() );
+                    //int index = std::stoi( v.second.get_child( "index" ).data() );
                     int replicates = std::stoi( v.second.get_child( "replicates" ).data() );
 
                     data.setReplicates( replicates );
@@ -95,7 +94,7 @@ namespace adio {
                 }
 
                 return true;
-        
+
             } catch ( std::exception& e ) {
                 // std::cout << boost::diagnostic_information( e );
                 throw e;
@@ -104,27 +103,27 @@ namespace adio {
         }
 
         /////////////////////
-        
+
         template<> bool
         protocols< protocol<> >::write_json( std::ostream& o, const protocols< protocol<> >& protocols )
         {
             boost::property_tree::ptree pt;
-    
+
             pt.put( "protocols.interval", protocols.interval_ * 1.0e6 ); // seconds --> us
-    
+
             boost::property_tree::ptree pv;
-    
+
             int protocolIndex( 0 );
-    
+
             for ( const auto& protocol: protocols.protocols_ ) {
-        
+
                 boost::property_tree::ptree xproto;
-        
+
                 xproto.put( "index", protocolIndex++ );
                 xproto.put( "replicates", protocol.replicates() );
-        
+
                 boost::property_tree::ptree xpulses;
-        
+
                 for ( const auto& pulse: protocol.pulses() ) {
                     boost::property_tree::ptree xpulse;
 
@@ -133,16 +132,16 @@ namespace adio {
 
                     xpulses.push_back( std::make_pair( "", xpulse ) );
                 }
-        
+
                 xproto.add_child( "pulses", xpulses );
 
                 pv.push_back( std::make_pair( "", xproto ) );
             }
-    
+
             pt.add_child( "protocols.protocol", pv );
 
             boost::property_tree::write_json( o, pt );
-    
+
             return true;
         }
 
