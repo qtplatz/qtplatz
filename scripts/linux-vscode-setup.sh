4c65275d302648013297d6c7629d3e1aea5655c4
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2019-12-09 08:46:16 +0900

    update scripts

--- scripts/linux-vscode-setup.sh
+++ scripts/linux-vscode-setup.sh
@@ -11,4 +11,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-sudo apt-get install code
+echo "--------------------------------------"
+echo "vscode repo was setup"
+echo "run sudo apt-get install code"
+echo "--------------------------------------"
