db4217e67352eb67f8b8fdf297fbe029312afcc8
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2020-02-29 17:28:31 +0900

    trial changes for mac and linux jupyter start script

--- scripts/jupyter-run.sh
+++ scripts/jupyter-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-__arch=`uname`
+os=`uname`
+arch=`uname`-`arch`
 config=
 cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -39,50 +40,41 @@ done
 echo "PYTHON_VERSION=" ${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}
 echo "config: " $config
 
-case "${__arch}" in
+case "${arch}" in
 	Linux*)
-		# for QtPlatz
 	    if [ "$config" = "debug" ]; then
-			__qtpath=("${HOME}/src/build-Linux-x86_64/qtplatz.release/python3.7")
-		else
-			__qtpath=("/usr/local/lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/dist-packages")
+			__search_path=("${HOME}/src/build-${arch}/qtplatz.release/python${PYTHON_VERSION}" )
 		fi
-		for dir in "${__qtpath[@]}"; do
-			if [ -d $dir/qtplatz ]; then
-				__PATH=$dir
-			fi
-		done
-		# for RDKit
-		__rdpath=("/usr/local/lib/python${PYTHON_VERSION_MAJOR}/dist-packages")
-		for dir in "${__rdpath[@]}"; do
-			if [ -d $dir/rdkit ]; then
-				__PATH=$__PATH:$dir
-			fi
-		done
+		__search_path+=("/usr/local/lib/python${PYTHON_VERSION}/dist-packages"
+						"/usr/local/lib/python${PYTHON_VERSION_MAJOR}/dist-packages" ) # RDKit py modules on Linux
 		;;
 	Darwin*)
 		if [ "$config" = "debug" ]; then
-			__search_path=("${HOME}/src/build-Darwin-i386/qtplatz.release/bin/qtplatz.app/Library/Python/3.7/site-packages")
+			__search_path=("${HOME}/src/build-${arch}/qtplatz.release/bin/qtplatz.app/Library/Python/${PYTHON_VERSION}/site-packages")
 		fi
-		__search_path+=("${HOME}/Desktop/qtplatz.app/Library/Python/3.7/site-packages"
-						"/usr/local/lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/site-packages")
-		for dir in "${__search_path[@]}"; do
-			if [ -d $dir/qtplatz ]; then
-				QTPLATZ_DIR=$dir
-				break
-			fi
-		done
-		for dir in "${__search_path[@]}"; do
-			if [ -d $dir/rdkit ]; then
-				RDKIT_DIR=$dir
-				break
-			fi
-		done
+		__search_path+=("${HOME}/Desktop/qtplatz.app/Library/Python/${PYTHON_VERSION}/site-packages"
+						"/Applications/qtplatz.app/Library/Python/${PYTHON_VERSION}/site-packages"
+						"/usr/local/lib/python${PYTHON_VERSION}/site-packages") # RDKit py modules on mac 
+		;;
 	    ;;
 	*)
-	    echo "######## unknown arch: " $__arch
+	    echo "######## unknown arch: " $arch
+		exit 1
 	    ;;
 esac
+
+for dir in "${__search_path[@]}"; do
+	if [ -d $dir/qtplatz ]; then
+		QTPLATZ_DIR=$dir
+		break
+	fi
+done
+for dir in "${__search_path[@]}"; do
+	if [ -d $dir/rdkit ]; then
+		RDKIT_DIR=$dir
+		break
+	fi
+done
 
 export PYTHONPATH=${QTPLATZ_DIR}:${RDKIT_DIR}
 echo "PYTHONPATH=" $PYTHONPATH
