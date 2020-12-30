94e7285f59552cb06129a1efb5fc283926a6bbd2
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2020-12-30 14:15:08 +0900

    minor change

--- cmake/Modules/Findqwt.cmake
+++ cmake/Modules/Findqwt.cmake
@@ -5,37 +5,32 @@ if ( qwt_FOUND )
 endif()
 
 if ( WIN32 )
-  find_path( qwt_DIR NAMES include/qwt.h HINTS
-    C:/qwt-6.1.5
-    C:/qwt-6.1.4-svn
-    C:/Qwt-6.1.3
-    $ENV{QWT} )
+  set ( __prefix "C:" )
 else()
-  find_path( qwt_DIR NAMES include/qwt.h HINTS
-    /usr/local/qwt-6.1.6-svn
-    /usr/local/qwt-6.1.5-svn
-    /usr/local/qwt-6.1.4-svn
-    /usr/local/qwt-6.1.3-svn
-    /usr/local/qwt-6.1.2-svn
-    /usr/local/qwt-6.1.2
-    $ENV{QWT}
-    )
+  set ( __prefix "/usr/local" )
 endif()
 
+set ( __qwt_dirs "" )
+foreach( __qwt_version "6.1.6" "6.1.5" "6.1.4" "6.1.3" "6.1.2" )
+  list ( APPEND __qwt_dirs "${__prefix}/qwt-${__qwt_version}" "${__prefix}/qwt-${__qwt_version}-svn" )
+endforeach()
+
+find_path( qwt_DIR NAMES include/qwt.h HINTS ${__qwt_dirs} "$ENV{QWT}" )
+
 if ( qwt_DIR )
-  set( QWT_INCLUDE_DIR ${qwt_DIR}/include )
-  set( QWT_INCLUDE_DIRS ${QWT_INCLUDE_DIR} )
+  set( QWT_INCLUDE_DIR "${qwt_DIR}/include" )
+  set( QWT_INCLUDE_DIRS "${QWT_INCLUDE_DIR}" )
 
   set( QWT_LIB "QWT_LIB-NOTFOUND" )
   set( QWT_DEBUG_LIB "QWT_DEBUG_LIB-NOTFOUND" )
 
   find_library( QWT_LIB NAMES qwt HINTS ${qwt_DIR}/lib )
-#  find_library( QWT_DEBUG_LIB NAMES qwt${CMAKE_DEBUG_POSTFIX} HINTS ${qwt_DIR}/lib )
+
   if ( QWT_LIB )
     add_library( qwt STATIC IMPORTED )
     set ( QWT_LIBRARIES qwt )
 
-    if ( WIN32 ) 
+    if ( WIN32 )
       set_target_properties( qwt PROPERTIES
 	IMPORTED_LOCATION ${qwt_DIR}/lib/qwt.lib
 	IMPORTED_LOCATION_DEBUG ${qwt_DIR}/lib/qwtd.lib
@@ -45,10 +40,6 @@ if ( qwt_DIR )
 	IMPORTED_LOCATION ${qwt_DIR}/lib/libqwt.a
 	)
     endif()
-    #set_target_properties( Qwt PROPERTIES IMPORTED_LOCATION ${QWT_LIB} )
-    #if ( QWT_DEBUG_LIB )
-    #  set_target_properties( Qwt PROPERTIES IMPORTED_LOCATION_DEBUG ${QWT_DEBUG_LIB} )
-    #endif()
     set( qwt_FOUND TRUE )
   else()
     message( FATAL_ERROR ${QWT_LIB} " " ${QWT_DLL} )
