dcac5bb1666a1a179659a494a5a85076ad22f9c8
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2022-02-08 19:57:36 +0900

    remove unused code

--- tests/csv_parser/main.cpp
+++ tests/csv_parser/main.cpp
@@ -1,85 +1,68 @@
+// https://stackoverflow.com/questions/50821925/spirit-x3-parser-with-internal-state
+#define BOOST_SPIRIT_X3_DEBUG
 #include <iostream>
-#include <variant>
-
+#include <iomanip>
 #include <boost/spirit/home/x3.hpp>
-#include <boost/spirit/home/support.hpp>
+#include <boost/variant.hpp>
 
-namespace helpers {
-    // https://bitbashing.io/std-visit.html
-    template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-    template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+namespace x3 = boost::spirit::x3;
+
+namespace CSV {
+    struct text    {};
+    struct integer {};
+    struct real    {};
+    struct skip    {};
+
+    struct handler {
+        template< typename T > void operator()( T& ctx ) {
+            std::cout << _attr(ctx) << std::endl;
+        }
+    };
+
+    auto const unquoted_text_field = *~x3::char_(",\n");
+    static inline auto as_parser(skip)    { return x3::omit[unquoted_text_field];  }
+    static inline auto as_parser(text)    { return unquoted_text_field[handler()]; }
+    static inline auto as_parser(integer) { return x3::int_[handler()];            }
+    static inline auto as_parser(real)    { return x3::double_[handler()];         }
+
+    template <typename... Spec>
+    static inline auto line_parser(Spec... spec) {
+        auto delim = x3::char_(",\t ") >> *x3::char_("\t ") | &(x3::eoi | x3::eol); // this ignores continuous comma ,,,
+#ifdef BOOST_SPIRIT_X3_DEBUG
+        auto debugged = [](auto spec) {
+            using boost::core::demangle_alloc; // leaked on purpose
+            return x3::rule<struct _> {demangle_alloc(typeid(spec).name())} = as_parser(spec);
+        };
+        return x3::rule<struct __> {"line"} = ((debugged(spec) >> delim) >> ... >> x3::eps);
+#else
+        return ((as_parser(spec) >> delim) >> ... >> x3::eps);
+#endif
+    }
+
+    template <typename... Spec> static inline auto csv_parser(Spec... spec) {
+        return line_parser(spec...) % x3::eol;
+    }
 }
 
-auto const unquoted_text_field = *(boost::spirit::x3::char_ - ',' - boost::spirit::x3::eol);
+using namespace CSV;
 
-struct text { };
-struct integer { };
-struct real { };
-struct skip { };
-typedef std::variant<text, integer, real, skip> column_variant;
+int
+main()
+{
+    //std::string const input = "Hello,1,13.7,XXX\nWorld,2,1e3,YYY";
+    std::string const input = "Hello\t,\t1,13.7,XXX\nWorld,2,1e3,YYY";
+    auto f = begin(input), l = end(input);
 
-struct column_value_parser : boost::spirit::x3::parser<column_value_parser> {
-    typedef boost::spirit::unused_type attribute_type;
+    //auto p = csv_parser(text{}, integer{}, real{}, skip{});
+    auto p = csv_parser( text{}, integer{}, real{}, skip{} );
 
-    std::vector<column_variant>& columns;
-    size_t mutable pos = 0;
-    struct pos_tag;
-
-    column_value_parser(std::vector<column_variant>& columns)
-        : columns(columns)
-    { }
-
-    template<typename It, typename Ctx, typename Other, typename Attr>
-    bool parse(It& f, It l, Ctx& /*ctx*/, Other const& /*other*/, Attr& /*attr*/) const {
-        auto const saved_f = f;
-        bool successful = false;
-
-        visit(
-            helpers::overloaded {
-                [&](skip const&) {
-                    successful = boost::spirit::x3::parse(f, l, boost::spirit::x3::omit[unquoted_text_field]);
-                },
-                [&](text&) {
-                    std::string value;
-                    successful = boost::spirit::x3::parse(f, l, unquoted_text_field, value);
-                    if(successful) {
-                        std::cout << "Text: " << value << '\n';
-                    }
-                },
-                [&](integer&) {
-                    int value;
-                    successful = boost::spirit::x3::parse(f, l, boost::spirit::x3::int_, value);
-                    if(successful) {
-                        std::cout << "Integer: " << value << '\n';
-                    }
-                },
-                [&](real&) {
-                    double value;
-                    successful = boost::spirit::x3::parse(f, l, boost::spirit::x3::double_, value);
-                    if(successful) {
-                        std::cout << "Real: " << value << '\n';
-                    }
-                }
-            },
-            columns[pos]);
-
-        if(successful) {
-            pos = (pos + 1) % columns.size();
-            return true;
-        } else {
-            f = saved_f;
-            return false;
-        }
+    if (parse(f, l, p)) {
+        std::cout << "Parsed\n";
+    } else {
+        std::cout << "Failed\n";
     }
-};
 
-
-int main() {
-    std::string input = "Hello,1,13.7,XXX\nWorld,2,1e3,YYY";
-
-    std::vector<column_variant> columns = {text{}, integer{}, real{}, skip{}};
-
-    boost::spirit::x3::parse(
-        input.begin(), input.end(),
-        (column_value_parser(columns) % ',') % boost::spirit::x3::eol);
+    if (f!=l) {
+        std::cout << "Remaining: " << std::quoted(std::string(f,l)) << "\n";
+    }
 }
