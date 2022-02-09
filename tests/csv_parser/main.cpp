0ba0a8b1e037893cd55601d277423642a892f0c6
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2022-02-09 10:24:23 +0900

    little progress on positional csv parser

--- tests/csv_parser/main.cpp
+++ tests/csv_parser/main.cpp
@@ -1,42 +1,56 @@
 // https://stackoverflow.com/questions/50821925/spirit-x3-parser-with-internal-state
-#define BOOST_SPIRIT_X3_DEBUG
+//#define BOOST_SPIRIT_X3_DEBUG
 #include <iostream>
 #include <iomanip>
 #include <boost/spirit/home/x3.hpp>
+#include <boost/spirit/home/x3/support/ast/variant.hpp>
 #include <boost/variant.hpp>
 
 namespace x3 = boost::spirit::x3;
+
+typedef boost::variant< std::string, int, double, boost::spirit::x3::unused_type > variant_type;
 
 namespace CSV {
     struct text    {};
     struct integer {};
     struct real    {};
     struct skip    {};
+    struct number  {};
+
+    struct visitor : boost::static_visitor< void > {
+        template< typename T > void operator()( T&& t ) const {
+            std::cout << "\t" << typeid( t ).name() << ":\t" << t << std::endl;
+        }
+        void operator()( double&& t ) const {
+            std::cout << "\tdouble:\t" << t << std::endl;
+        }
+        void operator()( int&& t ) const {
+            std::cout << "\tint:\t" << t << std::endl;
+        }
+        void operator()( std::string&& t ) const {
+            std::cout << "\tstring:\t'" << t << "'" << std::endl;
+        }
+    };
 
     struct handler {
-        template< typename T > void operator()( T& ctx ) {
-            std::cout << _attr(ctx) << std::endl;
+        template< typename T > void operator()( T& ctx ) const {
+            auto value = _attr(ctx);
+            // std::cout << typeid( value ).name() << "\t" << value << std::endl;
+            boost::apply_visitor( visitor(), variant_type( value ) );
         }
     };
 
     auto const unquoted_text_field = *~x3::char_(",\n");
     static inline auto as_parser(skip)    { return x3::omit[unquoted_text_field];  }
-    static inline auto as_parser(text)    { return unquoted_text_field[handler()]; }
-    static inline auto as_parser(integer) { return x3::int_[handler()];            }
-    static inline auto as_parser(real)    { return x3::double_[handler()];         }
+    static inline auto as_parser(text)    { return ( unquoted_text_field | x3::attr(0) ); }
+    static inline auto as_parser(integer) { return ( x3::int_    | x3::attr(0) );  }
+    static inline auto as_parser(real)    { return ( x3::double_ | x3::attr(0) );  }
+    static inline auto as_parser(number)  { return ( x3::double_ | x3::int_ | x3::attr(0) ); }
 
     template <typename... Spec>
     static inline auto line_parser(Spec... spec) {
         auto delim = x3::char_(",\t ") >> *x3::char_("\t ") | &(x3::eoi | x3::eol); // this ignores continuous comma ,,,
-#ifdef BOOST_SPIRIT_X3_DEBUG
-        auto debugged = [](auto spec) {
-            using boost::core::demangle_alloc; // leaked on purpose
-            return x3::rule<struct _> {demangle_alloc(typeid(spec).name())} = as_parser(spec);
-        };
-        return x3::rule<struct __> {"line"} = ((debugged(spec) >> delim) >> ... >> x3::eps);
-#else
-        return ((as_parser(spec) >> delim) >> ... >> x3::eps);
-#endif
+        return ((as_parser(spec) [ handler() ] >> delim) >> ... >> x3::eps);
     }
 
     template <typename... Spec> static inline auto csv_parser(Spec... spec) {
@@ -50,11 +64,12 @@ int
 main()
 {
     //std::string const input = "Hello,1,13.7,XXX\nWorld,2,1e3,YYY";
-    std::string const input = "Hello\t,\t1,13.7,XXX\nWorld,2,1e3,YYY";
+    std::string const input = "Hello\t,\t1,13.7,\"XXX\"\n\"World\",2,1e3,YYY\n,,,YYY";
     auto f = begin(input), l = end(input);
 
     //auto p = csv_parser(text{}, integer{}, real{}, skip{});
-    auto p = csv_parser( text{}, integer{}, real{}, skip{} );
+    //auto p = csv_parser( text{}, integer{}, real{}, skip{} );
+    auto p = csv_parser( text{}, number{}, number{}, skip{} );
 
     if (parse(f, l, p)) {
         std::cout << "Parsed\n";
