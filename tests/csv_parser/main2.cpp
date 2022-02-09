8cf978a13fe97158585ba317624775a6b746f682
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2022-02-09 16:54:06 +0900

    add more csv_parser test case

--- tests/csv_parser/main2.cpp
+++ tests/csv_parser/main2.cpp
@@ -5,11 +5,34 @@
 #include <boost/spirit/home/x3.hpp>
 #include <boost/spirit/home/x3/support/ast/variant.hpp>
 #include <boost/variant.hpp>
+#include <boost/format.hpp>
+#include <boost/filesystem.hpp>
+#include <fstream>
 
 namespace x3 = boost::spirit::x3;
 
 typedef boost::variant< boost::spirit::x3::unused_type, std::string, double, int > variant_type;
 typedef std::vector< variant_type > list_type;
+
+struct print_visitor : boost::static_visitor< void > {
+    template< typename T >
+    void operator()( T&& t ) const {
+    }
+
+    void operator()( const std::string& t ) const {
+        std::cout << t;
+    }
+    void operator()( int t ) const {
+        std::cout << boost::format( "%8d\t" ) % t;
+    }
+    void operator()( double t ) const {
+        if ( std::abs(t) < 0.001 || std::abs(t) > 10000 )
+            std::cout << boost::format( "%.5e\t" ) % t;
+        else
+            std::cout << boost::format( "%8.3f\t" ) % t;
+    }
+};
+
 
 namespace CSV {
 
@@ -58,30 +81,58 @@ namespace CSV {
 using namespace CSV;
 
 int
-main()
+main( int argc, const char * const argv[] )
 {
-    //std::string const input = "Hello,1,13.7,XXX\nWorld,2,1e3,YYY";
-    std::string const input = "Hello\tabc,,,\t999,13.7,,888abc,\t\"Hello\tWorld!\""; // \"World\",2,1e3,YYY\n,,,YYY";
-    auto first = begin(input), last = end(input);
+    if ( argc < 2 ) {
+        //std::string const input = "Hello,1,13.7,XXX\nWorld,2,1e3,YYY";
+        std::string const input = "Hello\tabc,,,\t999,13.7,,888abc,\t\"Hello\tWorld!\""; // \"World\",2,1e3,YYY\n,,,YYY";
+        auto first = begin(input), last = end(input);
 
-    auto p = csv_parser();
-    list_type list;
-    if ( x3::parse( first, last, p, list ) ) {
-        std::cout << "Parsed\n";
+        auto p = csv_parser();
+        list_type list;
+        if ( x3::parse( first, last, p, list ) ) {
+            std::cout << "Parsed\n";
 
-        size_t n(0);
-        for ( auto& value: list ) {
-            std::cout << "[" << n++ << "] " << value.which() << ", '" << value << "'" << std::endl;
-            if ( value.type() == typeid( std::string ) ) {
-                auto a = boost::get< std::string >( value );
-                if ( type_parser_list< integer, real, null >()( a, value ) )
-                    std::cout << "\treplace type: " << value.type().name() << "\t" << value << std::endl;
+            size_t n(0);
+            for ( auto& value: list ) {
+                std::cout << "[" << n++ << "] " << value.which() << ", '" << value << "'" << std::endl;
+                if ( value.type() == typeid( std::string ) ) {
+                    auto a = boost::get< std::string >( value );
+                    if ( type_parser_list< integer, real, null >()( a, value ) )
+                        std::cout << "\treplace type: " << value.type().name() << "\t" << value << std::endl;
+                }
             }
+        } else {
+            std::cout << "Failed\n";
+        }
+        if ( first != last) {
+            std::cout << "Remaining: " << std::quoted(std::string(first,last)) << "\n";
         }
     } else {
-        std::cout << "Failed\n";
-    }
-    if ( first != last) {
-        std::cout << "Remaining: " << std::quoted(std::string(first,last)) << "\n";
+        auto file = boost::filesystem::path( argv[ 1 ] );
+        if ( boost::filesystem::exists( file ) ) {
+            std::ifstream inf( file.string() );
+            std::string line;
+            while ( std::getline( inf, line ) ) {
+                auto first( std::begin( line ) ), last( std::end( line ) );
+                auto p = csv_parser();
+                list_type list;
+                if ( x3::parse( first, last, p, list ) ) {
+                    for ( auto& value: list ) {
+                        if ( value.type() == typeid( std::string ) ) {
+                            auto a = boost::get< std::string >( value );
+                            type_parser_list< integer, real, null >()( a, value );
+                            boost::apply_visitor( print_visitor{}, variant_type( value ) );
+                        }
+                    }
+                    std::cout << std::endl;
+                } else {
+                    std::cout << "\tFailed" << std::endl;
+                }
+                if ( first != last) {
+                    std::cout << "\tRemaining: " << std::quoted(std::string(first,last)) << std::endl;
+                }
+            }
+        }
     }
 }
