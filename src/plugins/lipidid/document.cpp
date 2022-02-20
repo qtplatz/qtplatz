70d5ffe0621ee172578a232570ce5c460d57870f
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2022-02-20 10:49:38 +0900

    more refactor

--- src/plugins/lipidid/document.cpp
+++ src/plugins/lipidid/document.cpp
@@ -68,18 +68,6 @@ namespace lipidid {
         }
     };
 
-
-    struct reference_mass {
-        typedef std::tuple< double, int, std::string, std::string > value_type;
-        value_type t_;
-        reference_mass( value_type&& t ) : t_( std::move( t ) ) {}
-        reference_mass( const reference_mass& t ) : t_( t.t_ ) {}
-        double mass() const { return std::get< 0 >( t_ ); }
-        int charge() const { return std::get< 1 >( t_ ); }
-        std::string formula() const { return std::get< 2 >( t_ ); }
-        std::string adducts() const { return std::get< 3 >( t_ ); }
-    };
-
     class document::impl {
     public:
         ~impl() {
@@ -99,19 +87,9 @@ namespace lipidid {
             }
         }
 
-        std::vector< std::string >
-        getInChIKeys( const std::string& formula ) const {
-            std::vector< std::string > t;
-            auto it = mols_.find( formula );
-            if ( it != mols_.end() ) {
-                for ( const auto& mol: it->second )
-                    t.emplace_back( mol.inchikey() );
-            }
-            return t;
-        }
-
-        std::shared_ptr< lipidid::simple_mass_spectrum > find_all( std::shared_ptr< const adcontrols::MassSpectrum >
-                                                                   , std::shared_ptr< adwidgets::ProgressInterface > ) const;
+        std::shared_ptr< lipidid::simple_mass_spectrum >
+        find_all( std::shared_ptr< const adcontrols::MassSpectrum >
+                  , std::shared_ptr< adwidgets::ProgressInterface > ) const;
 
         std::unique_ptr< QSettings > settings_;
         QSqlDatabase db_;
@@ -120,8 +98,6 @@ namespace lipidid {
         std::shared_ptr< const adcontrols::MassSpectrum > refms_;
         std::shared_ptr< lipidid::simple_mass_spectrum > simple_mass_spectrum_;
         adcontrols::MetIdMethod method_;
-        std::map< std::string, std::vector< lipidid::mol > > mols_; // stdformula, vector< mol >
-        std::vector< reference_mass > reference_list_;
     };
 
 }
@@ -278,15 +254,13 @@ document::getResultSet() const
 bool
 document::find_all( adcontrols::MetIdMethod&& t )
 {
-    qtwrapper::waitCursor waitCursor;
-
-    impl_->reference_list_.clear();
     impl_->method_ = std::move( t );
     if ( ! impl_->ms_ ) {
         ADDEBUG() << "no spectrum to be processed.";
         return false;
     }
     if ( impl_->ms_ ) {
+        qtwrapper::waitCursor waitCursor;
         auto p = std::make_shared< adwidgets::ProgressInterface >();
         Core::ProgressManager::addTask( p->progress.future(), "Processing...", Constants::LIPIDID_TASK_FIND_ALL );
 
@@ -304,134 +278,3 @@ document::find_all( adcontrols::MetIdMethod&& t )
 
     return true;
 }
-
-#if 0
-std::shared_ptr< lipidid::simple_mass_spectrum >
-document::impl::find_all( std::shared_ptr< const adcontrols::MassSpectrum > ms
-                          , std::shared_ptr< adwidgets::ProgressInterface > progress ) const
-{
-    double mass_tolerance = method_.tolerance( method_.toleranceMethod() );
-    ADDEBUG() << "mass tolerance: " << mass_tolerance * 1000 << "mDa";
-
-    using lipidid::simple_mass_spectrum;
-    using lipidid::mass_value_t;
-    auto tms = std::make_shared< lipidid::simple_mass_spectrum >();
-    tms->populate( *ms, [](auto value){ return mass_value_t::color( value ) == 15; });
-    if ( tms->size() == 0 ) {
-        ADDEBUG() << "no colored peak.";
-        return {};
-    }
-
-    (*progress)( 0, tms->size() );
-
-    ADDEBUG() << "populating mols from database...";
-    size_t counts(0);
-    if ( impl_->sqlite_ ) {
-        adfs::stmt sql( *impl_->sqlite_ );
-        sql.prepare( "SELECT id,formula,smiles,inchiKey FROM mols WHERE mass < 1200 ORDER BY mass" );
-        while ( sql.step() == adfs::sqlite_row ) {
-            ++counts;
-            auto [ id, formula, smiles, inchikey ] = adfs::get_column_values< int64_t, std::string, std::string, std::string >( sql );
-            impl_->mols_[ formula ].emplace_back( std::make_tuple( id, formula, smiles, inchikey ) );
-        }
-    }
-
-    (*progress)( 0, tms->size() );
-
-    ADDEBUG() << impl_->mols_.size() << " formulae loaded from " << counts << " total molecules";
-    ADDEBUG() << "generating reference mass list...";
-
-    for ( auto it = impl_->mols_.begin(); it != impl_->mols_.end(); ++it ) {
-        const auto& stdformula = it->first;
-        for ( auto adduct: method.adducts() ) {
-            if ( adduct.first ) { // enable
-                auto formulae = adcontrols::ChemicalFormula::split( stdformula + " " + adduct.second );
-                auto [mass, charge] = adcontrols::ChemicalFormula().getMonoIsotopicMass( formulae );
-                impl_->reference_list_.emplace_back( std::make_tuple( mass, charge, stdformula, adduct.second ) );
-            }
-        }
-    }
-
-    ADDEBUG() << impl_->reference_list_.size() << " total number of masses.";
-
-    // make it ascending order
-    std::sort( impl_->reference_list_.begin(), impl_->reference_list_.end()
-               , [](const auto& a, const auto& b){ return a.mass() < b.mass(); } );
-
-    // iterate observed peaks in the simple_mass_spectrum
-    for ( auto mIt = tms->begin(); mIt != tms->end(); ++mIt ) {
-        auto observed_mass = mass_value_t::mass( *mIt );
-        auto lit = std::lower_bound( impl_->reference_list_.begin()
-                                     , impl_->reference_list_.end()
-                                     , observed_mass - mass_tolerance / 2.0
-                                     , []( const auto& a, double b ){ return a.mass() < b; });
-
-        std::vector< std::vector< reference_mass >::const_iterator > tmp;
-        if ( lit != impl_->reference_list_.end() ) {
-            while ( lit->mass() < (observed_mass + mass_tolerance / 2.0) )
-                tmp.emplace_back( lit++ );
-        }
-        // sort gathered references in abs(mass error) ascending order
-        std::sort( tmp.begin(), tmp.end()
-                   , [&](const auto& a, const auto& b){
-                       return std::abs( observed_mass - a->mass() ) < std::abs( observed_mass - b->mass() );
-                   });
-        const size_t index = std::distance( tms->begin(), mIt ); // peak index
-
-        for ( const auto& t: tmp ) {
-            // isotope cluster
-            double mass         = t->mass();
-            std::string formula = t->formula();
-            std::string adducts = t->adducts();
-
-            auto cluster = isoCluster::compute( formula // ion_mass( *tIstd::get< 1 >( *tIt )->first // formula
-                                                , adducts     // *std::get< 2 >( *cIt )     // adduct
-                                                , 1.0e-4      // abundance low limit
-                                                , 4000 );     // R.P.
-
-            candidate x( mass               // exact mass
-                         , formula          // formula := map[string, mol]
-                         , adducts          // adduct
-                         , mass - mass_value_t::mass( (*tms)[ index ] ) // mass error
-                         , tms->find_cluster( index, cluster )
-                         , impl_->getInChIKeys( formula )
-                );
-            tms->add_a_candidate( index, std::move( x ) );
-        }
-    }
-    impl_->simple_mass_spectrum_ = std::move( tms );
-    // ADDEBUG() << "\n" << boost::json::object{{ "simple_mass_spectrum", *impl_->simple_mass_spectrum_ }};
-
-    if ( auto refMs = std::make_shared< adcontrols::MassSpectrum >() ) {
-        std::vector< double > masses, intensities;
-        refMs->clone( *ms, false );
-        auto simple_ms( impl_->simple_mass_spectrum_ );
-        for ( size_t idx = 0; idx < simple_ms->size(); ++idx ) {
-            auto [ tof, mass, intensity, color ] = (*simple_ms)[ idx ];
-
-            auto candidates = simple_ms->candidates( idx );
-            if ( ! candidates.empty() ) {
-                const auto& candidate = candidates.at( 0 );
-                auto cluster = isoCluster::compute( candidate.formula, candidate.adduct );
-                refMs->get_annotations()
-                    << adcontrols::annotation( candidate.formula + " " + candidate.adduct
-                                               , cluster.at(0).first // mass
-                                               , cluster.at(0).second * intensity
-                                               , masses.size() // index
-                                               , cluster.at(0).second * intensity
-                                               , adcontrols::annotation::dataFormula
-                                               , adcontrols::annotation::flag_targeting );
-                for ( const auto& ipk: cluster ) {
-                    masses.emplace_back( ipk.first );
-                    intensities.emplace_back( ipk.second * intensity );
-                }
-            }
-        }
-        refMs->setMassArray( std::move( masses ) );
-        refMs->setIntensityArray( std::move( intensities ) );
-        impl_->refms_ = std::move( refMs );
-    }
-
-    return tms;
-}
-#endif
