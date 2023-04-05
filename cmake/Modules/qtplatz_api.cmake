e0e45121e2d60af01ac3e0fb4dc7e402079a3346
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2023-04-05 09:41:05 +0900

    fix qt5 CMakeList.txt

--- cmake/Modules/qtplatz_api.cmake
+++ cmake/Modules/qtplatz_api.cmake
@@ -4,7 +4,11 @@ function( qtplatz_adplugin_output_dir varName provider )
   if ( APPLE )
     set( ${varName} "${CMAKE_BINARY_DIR}/bin/qtplatz.app/Contents/PlugIns/${provider}" PARENT_SCOPE) # := QTPLATZ_PLUGIN_DIRECTORY
   else()
-    set( ${varName} "${CMAKE_BINARY_DIR}/${QTC_BINARY_DIR}/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE) # := QTPLATZ_PLUGIN_DIRECTORY
+    if ( IDE_VERSION VERSION_GREATER_EQUAL "9.0.0" )
+      set( ${varName} "${CMAKE_BINARY_DIR}/${QTC_BINARY_DIR}/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE) # := QTPLATZ_PLUGIN_DIRECTORY
+    else()
+      set( ${varName} "${QTPLATZ_PLUGIN_DIRECTORY}/${provider}" PARENT_SCOPE) # := lib/qtplatz/plugins
+    endif()
   endif()
 
 endfunction()
