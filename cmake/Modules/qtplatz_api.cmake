f476ed2675ced60ba0b632789d4cec285a9dd002
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2023-04-11 11:51:42 +0900

    linux packager testing

--- cmake/Modules/qtplatz_api.cmake
+++ cmake/Modules/qtplatz_api.cmake
@@ -2,9 +2,13 @@
 function( qtplatz_adplugin_output_dir varName provider )
 
   if ( APPLE )
-    set( ${varName} "${CMAKE_BINARY_DIR}/bin/qtplatz.app/Contents/PlugIns/${provider}" PARENT_SCOPE) # := QTPLATZ_PLUGIN_DIRECTORY
+    # QTC_BINARY_DIR  := bin
+    # IDE_PLUGIN_PATH := qtplatz.app/Contents/PlugIns
+    set( ${varName} "${QTC_BINARY_DIR}/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE) # := QTPLATZ_PLUGIN_DIRECTORY
   else()
-    set( ${varName} "${CMAKE_BINARY_DIR}/${QTC_BINARY_DIR}/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE) # := QTPLATZ_PLUGIN_DIRECTORY
+    # QTC_BINARY_DIR  := qtplatz.app
+    # IDE_PLUGIN_PATH := lib/qtcreator/plugins
+    set( ${varName} "${QTC_BINARY_DIR}/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE) # := QTPLATZ_PLUGIN_DIRECTORY
   endif()
 
 endfunction()
