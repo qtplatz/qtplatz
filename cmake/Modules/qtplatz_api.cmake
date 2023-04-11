bdd030f83fe1db9fb050685c732190eecd0853f4
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2023-04-11 16:20:36 +0900

    change plugin install dir (exclude provider name if not apple)

--- cmake/Modules/qtplatz_api.cmake
+++ cmake/Modules/qtplatz_api.cmake
@@ -1,20 +1,34 @@
 
 function( qtplatz_adplugin_output_dir varName provider )
-
   if ( APPLE )
     set( ${varName} "bin/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE) # := QTPLATZ
   else()
-    set( ${varName} "${QTC_BINARY_DIR}/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE) # 'qtplatz'/'lib/qtcreator/plugins/'provider
+    set( ${varName} "${QTC_BINARY_DIR}/${IDE_PLUGIN_PATH}" PARENT_SCOPE) # 'qtplatz'/'lib/qtcreator/plugins ; /'provider
   endif()
-
 endfunction()
 
 function( qtplatz_adplugin_install_dir varName provider )
-
   if ( APPLE )
     set( ${varName} "bin/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE)
   else()
-    set( ${varName} "${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE)     # lib/qtcreator/plugins/ + provider
+    set( ${varName} "${IDE_PLUGIN_PATH}" PARENT_SCOPE)     # lib/qtcreator/plugins/ ; + provider
   endif()
+endfunction()
 
+################## plugin output ##############
+function( qtplatz_plugin_output_dir varName provider )
+  if ( APPLE )
+    set( ${varName} "bin/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE)
+  else()
+    set( ${varName} "${QTC_BINARY_DIR}/${IDE_PLUGIN_PATH}" PARENT_SCOPE)
+  endif()
+endfunction()
+
+################## plugin install ##############
+function( qtplatz_plugin_install_dir varName provider )
+  if ( APPLE )
+    set( ${varName} "bin/${IDE_PLUGIN_PATH}/${provider}" PARENT_SCOPE)
+  else()
+    set( ${varName} "${IDE_PLUGIN_PATH}" PARENT_SCOPE)
+  endif()
 endfunction()
