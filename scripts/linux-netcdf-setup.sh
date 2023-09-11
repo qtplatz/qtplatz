c86be07b477cf4d2e8d298b6bd1aef45c323ca6a
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2023-09-11 18:14:32 +0900

    exclude python library to avoid python dependency for adportable -- that causing problem on infitof build

---
+++ scripts/linux-netcdf-setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+cwd="$(cd "$(dirname "$0")" && pwd)"
+source ${cwd}/config.sh
+source ${cwd}/prompt.sh
+source ${cwd}/nproc.sh
+
+arch=`uname`-`arch`
+
+__nproc nproc
+
+build_clean=false
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		clean)
+			shift
+			build_clean=true
+			;;
+	esac
+done
+
+if [ -z $cross_target ]; then
+    BUILD_DIR=$SRC/build-$arch/netcdf-c.release
+    SRCDIR=$SRC/netcdf-c
+else
+    BUILD_DIR=$SRC/build-$cross_target/netcdf-c.release
+    CROSS_ROOT=/usr/local/arm-linux-gnueabihf
+    SRCDIR=$CROSS_ROOT/usr/local/netcdf-c
+    TOOLCHAIN=$(dirname $cwd)/toolchain-arm-linux-gnueabihf.cmake
+fi
+
+if [ $build_clean = true ]; then
+	set -x
+	rm -rf $BUILD_DIR
+	exit
+fi
+
+if [ `uname` == "Darwin" ]; then
+    cmake_args=( "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15"  # Catalina
+			   )
+fi
+
+if [ ! -z $cross_target ]; then
+    cmake_args+=( "-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN" "-DCMAKE_BUILD_TYPE=Release" )
+else
+	cmake_args+=( "-DCMAKE_BUILD_TYPE=Release" )
+fi
+
+if [ ! -d $SRCDIR ]; then
+    if [ ! -d $(dirname $SRCDIR) ]; then
+		mkdir -p $(dirname $SRCDIR)
+    fi
+    git clone https://github.com/schrodinger/netcdf-c $SRCDIR
+fi
+
+mkdir -p $BUILD_DIR;
+cd $BUILD_DIR;
+
+echo cmake "${cmake_args[@]}" $SRCDIR
+prompt
+cmake "${cmake_args[@]}" $SRCDIR
+
+echo "make -j${nproc}"
+prompt
+make -k -j${nproc}
+
+if [ $? -eq 0 ]; then
+	echo sudo make install
+	prompt
+	sudo make install
+fi
