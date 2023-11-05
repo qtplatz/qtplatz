09a2fcfe76816aa2ca292d2f874c8540d498be6d
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2023-11-05 16:17:28 +0900

    add quick boost_json test code

---
+++ tests/boost_json/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <boost/json.hpp>
+
+int
+main()
+{
+    std::cout << "Hello world" << std::endl;
+    int i = 1;
+    double d = 9.999;
+    std::pair< int, int > pair{ 2, 3 };
+    std::vector< int > v{ 4, 5 };
+
+    boost::json::value jv = {{ "boost_i", i }
+                             , { "boost_d", d }
+                             , { "boost_pair", v }
+    };
+    std::cout << jv << std::endl;
+
+}
