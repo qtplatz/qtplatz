49b539825dda6b6ba9335facefa05e1f45cbcf25
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2023-12-17 16:56:54 +0900

    modified:   src/plugins/chemistry/pubchemwnd.cpp

--- src/plugins/chemistry/pubchemwnd.cpp
+++ src/plugins/chemistry/pubchemwnd.cpp
@@ -80,7 +80,8 @@ PubChemWnd::PubChemWnd( QWidget * parent ) : QWidget( parent )
                                            , impl_( new impl{} )
 {
     if ( auto layout = new QVBoxLayout( this ) ) {
-        if ( auto edit = adwidgets::add_widget( layout, adwidgets::create_widget< QTextEdit >("pubchem") ) ) {
+        if ( auto edit =
+             adwidgets::add_widget( layout, adwidgets::create_widget< QTextEdit >("pubchem") ) ) {
             impl_->textDocument_ = std::make_unique< QTextDocument >( edit );
             edit->setDocument( impl_->textDocument_.get() );
             edit->setAcceptRichText( true );
@@ -88,7 +89,7 @@ PubChemWnd::PubChemWnd( QWidget * parent ) : QWidget( parent )
             connect( edit, &QTextEdit::selectionChanged, this, [=](){
                 auto cursor = edit->textCursor();
                 auto block = cursor.block();
-                ADDEBUG() << "block.number: " << block.blockNumber() << ", line#" << block.firstLineNumber();
+                //ADDEBUG() << "block.number: " << block.blockNumber() << ", line#" << block.firstLineNumber();
                 ADDEBUG() << block.text().toStdString();
             });
         }
@@ -105,7 +106,8 @@ PubChemWnd::handleReply( const QByteArray& ba, const QString& url )
         using adportable::json_helper;
         auto jv = boost::json::parse( ba.toStdString(), ec );
         if ( !ec ) {
-            if ( auto p = json_helper::find_pointer( jv, "/PropertyTable/Properties/0/CanonicalSMILES", ec ) ) {
+            if ( auto p =
+                 json_helper::find_pointer( jv, "/PropertyTable/Properties/0/CanonicalSMILES", ec ) ) {
                 smiles = p->kind() == boost::json::kind::string ? p->as_string() : "";
             }
         }
@@ -114,6 +116,7 @@ PubChemWnd::handleReply( const QByteArray& ba, const QString& url )
         auto cursor = edit->textCursor();
         cursor.insertBlock( QTextBlockFormat{} );
         cursor.insertText( QString("<url>%1</url>\n").arg( url ) );
+
         if ( auto table = cursor.insertTable( 1, 2 ) ) {
             {
                 auto cell = table->cellAt( 0, 0 );
@@ -124,7 +127,8 @@ PubChemWnd::handleReply( const QByteArray& ba, const QString& url )
             if ( !smiles.empty() ) {
                 auto cell = table->cellAt( 0, 1 );
                 auto cur = cell.firstCursorPosition();
-                if ( auto mol = std::unique_ptr< RDKit::RWMol >( RDKit::SmilesToMol( smiles ) ) ) {
+                if ( auto mol =
+                     std::unique_ptr< RDKit::RWMol >( RDKit::SmilesToMol( smiles ) ) ) {
                     std::string svg = adchem::drawing::toSVG( *mol );
                     QSvgRenderer renderer( QByteArray( svg.data(), svg.size() ) );
                     QImage image( 200, 200, QImage::Format_ARGB32 );
@@ -134,7 +138,10 @@ PubChemWnd::handleReply( const QByteArray& ba, const QString& url )
                 }
 #endif
             }
+            // cursor = table->cellAt( 0, 0 ).lastCursorPosition();
         }
-        // cursor.insertText( "\n----- END PUG REST -----\n" );
+
+        cursor.movePosition( QTextCursor::Down );
+        cursor.insertText( "\n----- END PUG REST -----\n" );
     }
 }
